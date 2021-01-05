2ea6f273af7ce3fe17c4fcfe9feae7add961cca3
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2021-01-04 20:20:00 -0500

    feat: add cli entrypoint

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 extras_require = {
@@ -19,7 +19,15 @@ setup(
     url="https://apeworx.io",
     author="ApeWorX Team",
     author_email="admin@apeworx.io",
-    packages=[],
+    packages=find_packages(),
+    install_requires=[
+        "click>=7.1.2",
+    ],
+    entry_points={
+        "console_scripts": ["ape=ape._cli.__init__:cli"],
+    },
+    include_package_data=True,
+    python_requires=">=3.6,<4",
     extras_require=extras_require,
     classifiers=["Development Status :: 1 - Planning"],
 )
