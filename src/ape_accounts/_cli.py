b4f6b9e90db71fdc33122ea9918be15c5557700d
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2021-04-06 22:09:06 -0400

    refactor: update accounts cli to use notify

--- src/ape_accounts/_cli.py
+++ src/ape_accounts/_cli.py
@@ -5,6 +5,7 @@ from eth_account import Account as EthAccount  # type: ignore
 from eth_utils import to_bytes
 
 from ape import DATA_FOLDER, accounts
+from ape.utils import notify
 
 
 @click.group(short_help="Manage local accounts")
@@ -19,7 +20,7 @@ def cli():
 @cli.command(name="list", short_help="List available accounts")
 def _list():
     if len(accounts) == 0:
-        click.echo("No accounts found.")
+        notify("WARNING", "No accounts found.")
         return
 
     elif len(accounts) > 1:
@@ -36,7 +37,9 @@ def _list():
 @cli.command(short_help="Add a new account with a random private key")
 @click.argument("alias")
 def generate(alias):
-    assert alias not in accounts.aliases
+    if alias in accounts.aliases:
+        notify("ERROR", f"Account with alias '{alias}' already exists")
+        return
 
     path = DATA_FOLDER.joinpath(f"{alias}.json")
     extra_entropy = click.prompt(
@@ -51,7 +54,7 @@ def generate(alias):
     )
     path.write_text(json.dumps(EthAccount.encrypt(a.key, passphrase)))
 
-    click.echo(f"A new account '{a.address}' has been added with the id '{alias}'")
+    notify("SUCCESS", f"A new account '{a.address}' has been added with the id '{alias}'")
 
 
 # Different name because `import` is a keyword
@@ -59,7 +62,7 @@ def generate(alias):
 @click.argument("alias")
 def _import(alias):
     if alias in accounts.aliases:
-        click.echo(f"Account with alias '{alias}' already exists")
+        notify("ERROR", f"Account with alias '{alias}' already exists")
         return
 
     path = DATA_FOLDER.joinpath(f"{alias}.json")
@@ -72,7 +75,7 @@ def _import(alias):
     )
     path.write_text(json.dumps(EthAccount.encrypt(a.key, passphrase)))
 
-    click.echo(f"A new account '{a.address}' has been added with the id '{alias}'")
+    notify("SUCCESS", f"A new account '{a.address}' has been added with the id '{alias}'")
 
 
 @cli.command(short_help="Change the password of an existing account")
@@ -80,7 +83,7 @@ def _import(alias):
 def change_password(alias):
     account = accounts.load(alias)
     account.change_password()
-    click.echo(f"Password has been changed for account '{alias}'")
+    notify("SUCCESS", f"Password has been changed for account '{alias}'")
 
 
 @cli.command(short_help="Delete an existing account")
@@ -88,4 +91,4 @@ def change_password(alias):
 def delete(alias):
     account = accounts.load(alias)
     account.delete()
-    click.echo(f"Account '{alias}' has been deleted")
+    notify("SUCCESS", f"Account '{alias}' has been deleted")
