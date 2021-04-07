0616c9c1d1c4907595cbdc0b93c5a6fe1881e61a
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2021-04-06 21:10:32 -0400

    refactor: reduce clutter with console unless `--verbose` flag is present (#14)

--- src/ape_console/__init__.py
+++ src/ape_console/__init__.py
@@ -7,30 +7,36 @@ from ape.version import version as ape_version  # type: ignore
 
 
 @click.command(short_help="Load the console", context_settings=dict(ignore_unknown_options=True))
+@click.option("--verbose", is_flag=True, flag_value=True, default=False)
 @click.argument("ipython_args", nargs=-1, type=click.UNPROCESSED)
-def cli(ipython_args):
+def cli(verbose, ipython_args):
     """
     Opens a console for the local project."""
-    return console(ipython_args=ipython_args)
+    return console(verbose=verbose, ipython_args=ipython_args)
 
 
-def console(project=None, extra_locals=None, ipython_args=None):
+def console(project=None, verbose=False, extra_locals=None, ipython_args=None):
     if not project:
         # Use default project
         project = ape.project
 
     config = load_default_config()
-    config.TerminalInteractiveShell.banner1 = """
+
+    if verbose:
+        config.TerminalInteractiveShell.banner1 = """
    Python:  {python_version}
   IPython:  {ipython_version}
       Ape:  {ape_version}
   Project:  {project_path}
-""".format(
-        python_version=ape._python_version,
-        ipython_version=IPython.__version__,
-        ape_version=ape_version,
-        project_path=project._path,
-    )
+    """.format(
+            python_version=ape._python_version,
+            ipython_version=IPython.__version__,
+            ape_version=ape_version,
+            project_path=project._path,
+        )
+
+    else:
+        config.TerminalInteractiveShell.banner1 = ""
 
     namespace = {component: getattr(ape, component) for component in ape.__all__}
     if extra_locals:
