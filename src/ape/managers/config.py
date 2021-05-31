84d849162de1609fe4a9b6d159591f98f61b6bc4
unparalleled-js <yingthi@live.com>
2021-05-31 13:44:03 -0500

    Also set to empty dict if load_config returns None

--- src/ape/managers/config.py
+++ src/ape/managers/config.py
@@ -26,7 +26,7 @@ class ConfigManager:
         config_file = self.PROJECT_FOLDER / CONFIG_FILE_NAME
 
         if config_file.exists():
-            user_config = load_config(config_file)
+            user_config = load_config(config_file) or {}
         else:
             user_config = {}
 
