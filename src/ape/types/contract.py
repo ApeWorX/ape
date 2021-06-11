ac081af9c408f9618f5605fad35f2bf198502793
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-06-11 18:46:29 -0400

    feat: add selector and signature properties to ABI dataclasses

--- src/ape/types/contract.py
+++ src/ape/types/contract.py
@@ -88,6 +88,40 @@ class ABI(SerializableType):
     type: str
 
     @property
+    def signature(self) -> str:
+        """
+        String representing the function/event signature, which includes the arg names and types,
+        and output names (if any) and type(s)
+        """
+        name = self.name if (self.type == "function" or self.type == "event") else self.type
+
+        def encode_arg(arg: ABIType) -> str:
+            encoded_arg = arg.canonical_type
+            encoded_arg += " "
+            # For events (handles both None and False conditions)
+            if arg.indexed:
+                encoded_arg += "indexed "
+            encoded_arg += arg.name
+            return encoded_arg
+
+        input_args = ", ".join(map(encode_arg, self.inputs))
+        output_args = ""
+
+        if self.outputs:
+            output_args = " -> (" + ", ".join(map(encode_arg, self.outputs)) + ")"
+
+        return f"{name}({input_args}){output_args}"
+
+    @property
+    def selector(self) -> str:
+        """
+        String representing the function selector, used to compute `method_id` and `event_id`
+        """
+        name = self.name if (self.type == "function" or self.type == "event") else self.type
+        input_names = ", ".join(i.canonical_type for i in self.inputs)
+        return f"{name}({input_names})"
+
+    @property
     def is_event(self) -> bool:
         return self.anonymous is not None
 
@@ -155,11 +189,11 @@ class ContractType(FileMixin, SerializableType):
 
     @property
     def calls(self) -> List[ABI]:
-        return [abi for abi in self.abi if abi.type == "function" and abi.is_stateful]
+        return [abi for abi in self.abi if abi.type == "function" and not abi.is_stateful]
 
     @property
     def transactions(self) -> List[ABI]:
-        return [abi for abi in self.abi if abi.type == "function" and not abi.is_stateful]
+        return [abi for abi in self.abi if abi.type == "function" and abi.is_stateful]
 
     @classmethod
     def from_dict(cls, params: Dict):
