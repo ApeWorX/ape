0cb6d2138998f9025a02252a630b1ff8d603b390
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-06-30 18:11:19 -0400

    refactor: process scripts in a nicer way

--- src/ape_run/__init__.py
+++ src/ape_run/__init__.py
@@ -2,25 +2,10 @@ from pathlib import Path
 
 import click
 
-from ape.utils import Abort, notify
+from ape.utils import Abort
 
 
-def _run_script(scripts_folder, name, interactive=False):
-    if Path(name).exists():
-        # NOTE: This injects the path as the only option (bypassing the lookup)
-        script_file = Path(name)
-
-    else:  # We're using the short name, so generate the lookup table of all scripts in project
-        # NOTE: Generate the lookup based on all the scripts defined in the project
-        found_name = False
-        for script_file in scripts_folder.glob("*.py"):
-            if script_file.stem == name:
-                found_name = True
-                break  # NOTE: `script_file` is set
-
-        if not found_name:
-            raise Abort(f"No script named '{name}' detected in scripts folder")
-
+def _run_script(script_file, interactive=False):
     # Load the python module and find our hook functions
     ns: dict = {}
     try:
@@ -59,15 +44,29 @@ def cli(scripts, interactive):
     will be injected dynamically during script execution. The dynamically injected objects are
     the exports from the `ape` top-level package (similar to how the console works)
     """
-    from ape import config
-
-    scripts_folder = config.PROJECT_FOLDER / "scripts"
-    if not scripts_folder.exists():
-        notify("WARNING", "No `scripts/` directory detected")
-        return
-
     if len(scripts) == 0:
         raise Abort("Must provide at least one script name or path")
 
+    from ape import config
+
+    scripts_folder = config.PROJECT_FOLDER / "scripts"
+
+    # Generate the lookup based on all the scripts defined in the project's `scripts/` folderi
+    # NOTE: If folder does not exist, this will be empty (same as if there are no files)
+    available_scripts = {p.stem: p for p in scripts_folder.glob("*.py")}
+
     for name in scripts:
-        _run_script(scripts_folder, name, interactive)
+        if Path(name).exists():
+            # NOTE: This injects the path as the only option (bypassing the lookup)
+            script_file = Path(name)
+
+        elif not scripts_folder.exists():
+            raise Abort("No `scripts/` directory detected to run script")
+
+        elif name not in available_scripts:
+            raise Abort(f"No script named '{name}' detected in scripts folder")
+
+        else:
+            script_file = available_scripts[name]
+
+        _run_script(script_file, interactive)
--- tests/integration/cli/test_run.py
+++ tests/integration/cli/test_run.py
@@ -1,12 +1,18 @@
-def test_run(ape_cli, runner, project):
-    if not (project.path / "scripts").exists():
-        result = runner.invoke(ape_cli, ["run"])
-        assert result.exit_code == 0
-        assert "WARNING: No `scripts/` directory detected" in result.output
-        return  # Nothing else to test for this project
+BAD_COMMAND = "not-a-name"
 
+
+def test_run(ape_cli, runner, project):
     result = runner.invoke(ape_cli, ["run"])
     assert result.exit_code == 1
+    assert "Must provide at least one script name or path" in result.output
+
+    result = runner.invoke(ape_cli, ["run", BAD_COMMAND])
+    assert result.exit_code == 1
+    if not (project.path / "scripts").exists():
+        assert "No `scripts/` directory detected to run script" in result.output
+
+    else:
+        assert f"No script named '{BAD_COMMAND}' detected in scripts folder" in result.output
 
     for script_file in (project.path / "scripts").glob("*.py"):
         result = runner.invoke(ape_cli, ["run", script_file.stem])
