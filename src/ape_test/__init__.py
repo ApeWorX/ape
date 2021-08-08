5e24cf20d6a1c4d530bb029b0bb692391e198be8
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2021-08-08 11:54:44 -0400

    fix: remove unused network type after geth --dev was added (#123)

--- src/ape_test/__init__.py
+++ src/ape_test/__init__.py
@@ -1,12 +1,6 @@
 from ape import plugins
-from ape.api import create_network_type
 
 from .providers import LocalNetwork
-
-
-@plugins.register(plugins.NetworkPlugin)
-def networks():
-    yield "ethereum", "development", create_network_type(chain_id=69, network_id=69)
 
 
 @plugins.register(plugins.ProviderPlugin)
