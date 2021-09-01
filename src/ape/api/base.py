30b615837f951c8f11d5f67d5484a614023aa9df
Ser Doggo <3859395+fubuloubu@users.noreply.github.com>
2021-09-01 17:24:46 -0400

    fix: solve RLock bug by not pickling cached_property

--- src/ape/api/base.py
+++ src/ape/api/base.py
@@ -4,8 +4,18 @@ from typing import List
 
 from pydantic import BaseModel
 
+from ape.utils import cached_property
+
 
 class API(ABC, BaseModel):
+    class Config:
+        keep_untouched = (cached_property,)  #
+
+    # NOTE: Due to https://github.com/samuelcolvin/pydantic/issues/1241
+    #       we have to add this cached property workaround in order to avoid this error:
+    #
+    #           TypeError: cannot pickle '_thread.RLock' object
+
     def __dir__(self) -> List[str]:
         # Filter out private members
         return [member for member in super().__dir__() if not member.startswith("_")]
