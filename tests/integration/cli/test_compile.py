66b27d682a0d8e2fdb6aebbad228d74de16fff64
unparalleled-js <yingthi@live.com>
2021-10-07 12:46:07 -0500

    fix: try fix flakey test

--- tests/integration/cli/test_compile.py
+++ tests/integration/cli/test_compile.py
@@ -9,9 +9,9 @@ def test_compile(ape_cli, runner, project):
     if ".test" in project.extensions_with_missing_compilers:
         result = runner.invoke(ape_cli, ["compile"])
         assert result.exit_code == 0
-        assert (
-            "WARNING: No compilers detected for the " "following extensions: .test, .foobar"
-        ) in result.output
+        assert "WARNING: No compilers detected for the " "following extensions:" in result.output
+        assert ".test" in result.output
+        assert ".foobar" in result.output
 
         result = runner.invoke(ape_cli, ["compile", "contracts/Contract.test"])
         assert result.exit_code == 0
