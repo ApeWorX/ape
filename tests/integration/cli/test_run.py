cf8429bc03aeab6d59ba44c78527d91d8454d6ca
unparalleled-js <yingthi@live.com>
2021-10-29 15:40:40 -0500

    chore: fix tests

--- tests/integration/cli/test_run.py
+++ tests/integration/cli/test_run.py
@@ -2,11 +2,11 @@ BAD_COMMAND = "not-a-name"
 
 
 def test_run(ape_cli, runner, project):
-    result = runner.invoke(ape_cli, ["run"])
+    result = runner.invoke(ape_cli, ["run", "--network", "ethereum:development:test"])
     assert result.exit_code == 1
     assert "Must provide at least one script name or path" in result.output
 
-    result = runner.invoke(ape_cli, ["run", BAD_COMMAND])
+    result = runner.invoke(ape_cli, ["run", BAD_COMMAND, "--network", "ethereum:development:test"])
     assert result.exit_code == 1
     if not (project.path / "scripts").exists():
         assert "No 'scripts/' directory detected to run script" in result.output
