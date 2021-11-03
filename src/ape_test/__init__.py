c4a9fcceba93e79fb756290e73383726cce6ed6a
unparalleled-js <yingthi@live.com>
2021-11-03 12:09:50 -0500

    feat: test config

--- src/ape_test/__init__.py
+++ src/ape_test/__init__.py
@@ -1,6 +1,17 @@
 from ape import plugins
+from ape.api.config import ConfigItem
 
 from .providers import LocalNetwork
+
+
+class Config(ConfigItem):
+    mnemonic: str = "test test test test test test test test test test test junk"
+    number_of_accounts: int = 10
+
+
+@plugins.register(plugins.Config)
+def config_class():
+    return Config
 
 
 @plugins.register(plugins.ProviderPlugin)
