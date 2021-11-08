16303b3a2f143ca146687c7c87b5f2571b716425
sabotage beats <27985126+sabotagebeats@users.noreply.github.com>
2021-11-08 13:38:21 -0800

    docs: fixed linting

--- src/ape/api/accounts.py
+++ src/ape/api/accounts.py
@@ -43,10 +43,10 @@ class AccountAPI(AddressAPI):
     def sign_message(self, msg: SignableMessage) -> Optional[MessageSignature]:
         """
         Signs the given message.
-        
+
         Args:
           msg (:class:`~eth_account.messages.SignableMessage`): The message to sign.
-        
+
         Returns:
           :class:`~ape.types.signatures.MessageSignature` (optional): The signed message.
         """
@@ -55,10 +55,10 @@ class AccountAPI(AddressAPI):
     def sign_transaction(self, txn: TransactionAPI) -> Optional[TransactionSignature]:
         """
         Signs the given transaction.
-        
+
         Args:
           txn (:class:`~ape.api.providers.TransactionAPI`): The transaction to sign.
-        
+
         Returns:
           :class:`~ape.types.signatures.TransactionSignature` (optional): The signed transaction.
         """
