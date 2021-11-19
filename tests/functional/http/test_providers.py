f4ccab4bb96b1ff91426927820a047e0e0ac22cc
unparalleled-js <yingthi@live.com>
2021-11-19 10:25:57 -0600

    feat: handle no revert message

--- tests/functional/http/test_providers.py
+++ tests/functional/http/test_providers.py
@@ -70,3 +70,24 @@ class TestEthereumProvider:
             provider.send_transaction(mock_transaction)
 
         assert str(err.value) == _TEST_REVERT_REASON
+
+    def test_send_transaction_no_revert_message(
+        self, mock_web3, mock_network_api, mock_config_item, mock_transaction
+    ):
+        provider = GethProvider(
+            name="test",
+            network=mock_network_api,
+            config=mock_config_item,
+            provider_settings={},
+            data_folder=Path("."),
+            request_header="",
+        )
+        provider._web3 = mock_web3
+        mock_network_api.ecosystem.receipt_class = _create_mock_receipt()
+        test_err = Web3ContractLogicError("execution reverted")
+        mock_web3.eth.send_raw_transaction.side_effect = test_err
+
+        with pytest.raises(ContractLogicError) as err:
+            provider.send_transaction(mock_transaction)
+
+        assert str(err.value) == TransactionError.DEFAULT_MESSAGE
