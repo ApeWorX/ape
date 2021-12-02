ac69b82a70a3b5ef96b1b84bb8e837247f8a5f89
unparalleled-js <yingthi@live.com>
2021-12-02 09:51:01 -0600

    chore: add typing ext to setup.py

--- setup.py
+++ setup.py
@@ -86,6 +86,7 @@ setup(
         "IPython>=7.25",
         "pytest>=6.0,<7.0",
         "rich>=10.14,<11",
+        "typing-extensions ; python_version<'3.8'",
         "web3[tester]>=5.25.0,<6.0.0",
     ],
     entry_points={
