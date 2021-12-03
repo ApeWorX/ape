12699e22ab735659a73ffabde8fd154f3620242b
unparalleled-js <yingthi@live.com>
2021-12-03 16:34:14 -0600

    fix: call github api less in tests

--- tests/integration/cli/test_misc.py
+++ tests/integration/cli/test_misc.py
@@ -1,5 +1,7 @@
 import pytest
 
+from .utils import skip_projects_except
+
 
 # NOTE: test all the things without a direct test elsewhere
 @pytest.mark.parametrize(
@@ -13,10 +15,23 @@ import pytest
         ["networks"],
         ["networks", "list"],
         ["plugins"],
+    ),
+)
+def test_invocation(ape_cli, runner, args):
+    result = runner.invoke(ape_cli, args)
+    assert result.exit_code == 0
+
+
+# Only run these tests once (limited to single arbitrary project).
+# This is to prevent Github from rate limiting us and causing test failures.
+@skip_projects_except(["empty-config"])
+@pytest.mark.parametrize(
+    "args",
+    (
         ["plugins", "list"],
         ["plugins", "list", "--all"],
     ),
 )
-def test_invocation(ape_cli, runner, args):
+def test_invocation_run_once(ape_cli, runner, args):
     result = runner.invoke(ape_cli, args)
     assert result.exit_code == 0
