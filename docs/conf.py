dcf658cc670c71f8e66322057a4db346f282269f
Juliya Smith <yingthi@live.com>
2021-12-10 22:45:23 -0600

    fix: LTD
    Co-authored-by: El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "ape"
-copyright = "2021, ApeWorX"
+copyright = "2021, ApeWorX LTD"
 author = "ApeWorX"
 extensions = [
     "recommonmark",
