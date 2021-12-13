1c01536967af7be0ac352cbec9f6d61254a27cc7
unparalleled-js <yingthi@live.com>
2021-12-13 16:00:13 -0600

    fix: only build into a temp dir before copying

--- build_docs.py
+++ build_docs.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+import tempfile
 from pathlib import Path
 
 REDIRECT_HTML = """
@@ -61,23 +62,18 @@ def build_docs_from_release():
     if not tag:
         raise DocsBuildError("Unable to find release tag.")
 
-    if not LATEST_PATH.exists():
-        # This should already exist from the last push to 'main'.
-        # But just in case, we can build it now.
-        build_docs(LATEST_PATH)
+    with tempfile.TemporaryDirectory as temp_dir_str:
+        temp_dir = Path(temp_dir_str)
+        build_docs(temp_dir)
 
-    # Copy the latest build from 'main' to the new version dir.
-    shutil.copytree(LATEST_PATH, DOCS_BUILD_PATH / tag)
+        # Copy the latest build from 'main' to the new version dir.
+        shutil.copytree(temp_dir, DOCS_BUILD_PATH / tag)
+        shutil.copytree(temp_dir, STABLE_PATH)
 
-    # Copy the latest build from 'main' to the 'stable' dir.
-    if STABLE_PATH.exists():
-        shutil.rmtree(STABLE_PATH)
-    shutil.copytree(LATEST_PATH, STABLE_PATH)
-
-    # Clean-up unnecessary extra 'fonts/' directories to save space.
-    for font_dirs in DOCS_BUILD_PATH.glob("**/fonts"):
-        if font_dirs.exists():
-            shutil.rmtree(font_dirs)
+        # Clean-up unnecessary extra 'fonts/' directories to save space.
+        for font_dirs in DOCS_BUILD_PATH.glob("**/fonts"):
+            if font_dirs.exists():
+                shutil.rmtree(font_dirs)
 
 
 def main():
