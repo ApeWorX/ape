8f878c3367e0c2fcc607bc121dfd6a20f5864cb8
unparalleled-js <yingthi@live.com>
2021-12-14 09:19:39 -0600

    fix: make picker work on release

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -1,12 +1,18 @@
+function getSelectedDocsVersion() {
+  return document.location.pathname
+      .replace(PROJECT, "")
+      .replaceAll("/", "");
+}
+
 $(document).ready(function () {
   // Version picker logic
-  let current = document.location.pathname.replaceAll("/", "")
+  let current = getSelectedDocsVersion()
   $("option[value='" + current + "']").attr("selected", "selected");
   $("select").change(function () {
     if (this.value === "") {
       return false;
     }
-    let current = document.location.pathname.replaceAll("/", "")
+    let current = getSelectedDocsVersion()
     let selected = $(this).val();
     $("option[value='" + selected + "']").attr("selected", "selected");
     window.location = document.URL.replace(current, selected);
