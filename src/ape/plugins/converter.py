d57d24a8313d1b716e63640337ab90b19c1ba961
unparalleled-js <yingthi@live.com>
2021-12-23 13:03:00 -0600

    docs: redo

--- src/ape/plugins/converter.py
+++ src/ape/plugins/converter.py
@@ -6,8 +6,16 @@ from .pluggy_patch import PluginType, hookspec
 
 
 class ConversionPlugin(PluginType):
+    """
+    A plugin for converting values. The `ape-ens <https://github.com/ApeWorX/ape-ens>`__
+    plugin is an example of a conversion-plugin.
+    """
+
     @hookspec
     def converters(self) -> Iterator[Tuple[str, Type[ConverterAPI]]]:
         """
-        Must return an iterator of tuples of a string ABIType and an ConverterAPI subclass
+        A hook that returns an iterator of tuples of a string ABIType and an ConverterAPI subclass.
+
+        Returns:
+            iter[tuple[str, type[:class:`~ape.api.convert.ConverterAPI`]]]
         """
--- src/ape/plugins/network.py
+++ src/ape/plugins/network.py
@@ -6,25 +6,50 @@ from .pluggy_patch import PluginType, hookspec
 
 
 class EcosystemPlugin(PluginType):
+    """
+    An ecosystem plugin, such as `ape-ethereum`. See the
+    :class:`ape.api.networks.EcosystemAPI` for more information on
+    what is required to implement this type of plugin.
+    """
+
     @hookspec
     def ecosystems(self) -> Iterator[Type[EcosystemAPI]]:
         """
-        Must return an iterator of EcosystemAPI subclasses
+        A hook that must return an iterator of :class:`ape.api.networks.EcosystemAPI`
+        subclasses.
+
+        Returns:
+            iter[type[:class:`~ape.api.networks.EcosystemAPI`]]
         """
 
 
 class NetworkPlugin(PluginType):
+    """
+    A network plugin, such as `mainnet` or `ropsten`. Likely, registering networks
+    will happen soon after registering the ecosystem, as an ecosystem requires
+    networks.
+    """
+
     @hookspec
     def networks(self) -> Iterator[Tuple[str, str, Type[NetworkAPI]]]:
         """
-        Must return an iterator of tuples of:
-            the target Ecosystem plugin's name
-            the Network name
-            a NetworkAPI subclass
+        A hook that must return an iterator of tuples of:
+            * the target Ecosystem plugin's name
+            * the Network name
+            * a :class:`ape.api.networks.NetworkAPI` subclass
+
+        Returns:
+            iter[tuple[str, str, type[:class:`~ape.api.networks.NetworkAPI`]]]
         """
 
 
 class ProviderPlugin(PluginType):
+    """
+    A plugin representing a blockchain provider, which is the API responsible
+    for interacting with the blockchain, such as making network requests.
+    Example provider plugins would be
+    """
+
     @hookspec
     def providers(self) -> Iterator[Tuple[str, str, Type[ProviderAPI]]]:
         """
