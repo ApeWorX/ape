a144958035d084937be8638d1072bad67b7ff5d4
unparalleled-js <yingthi@live.com>
2022-01-04 18:00:22 -0600

    test: add test for verifying signing

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -7,3 +7,18 @@ from ape import networks
 def eth_tester_provider():
     with networks.parse_network_choice("::test"):
         yield networks.active_provider
+
+
+@pytest.fixture
+def test_accounts(accounts):
+    return accounts.test_accounts
+
+
+@pytest.fixture
+def sender(test_accounts):
+    return test_accounts[0]
+
+
+@pytest.fixture
+def receiver(test_accounts):
+    return test_accounts[1]
--- tests/integration/test_accounts.py
+++ tests/integration/test_accounts.py
@@ -1,7 +1,24 @@
+from eth_account import Account
+from eth_account.messages import encode_defunct
+
 from ape import accounts, convert
 
 
-def test_transfer(eth_tester_provider):
+def test_sign_message(test_accounts):
+    signer = test_accounts[2]
+    message = "I♥SF"
+    message_hash = encode_defunct(text=message)
+
+    actual_signature = signer.sign_message(message_hash)
+    actual_signature_bytes = actual_signature.encode_rsv()
+    expected_signature = Account.sign_message(message_hash, signer._private_key)
+    assert actual_signature_bytes == expected_signature.signature
+
+    signer_address = Account.recover_message(message_hash, signature=actual_signature_bytes)
+    assert signer_address == signer.address
+
+
+def test_transfer(eth_tester_provider, sender, receiver):
     sender = accounts.test_accounts[0]
     receiver = accounts.test_accounts[1]
     initial_balance = receiver.balance
