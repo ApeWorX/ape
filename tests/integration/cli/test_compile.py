66f64e1172b6150f398e7da2f075484f288bca91
Evan Blank <32708219+evanblank3@users.noreply.github.com>
2022-01-07 11:46:52 -0500

    refactor: remove test manifest folder

--- tests/integration/cli/test_compile.py
+++ tests/integration/cli/test_compile.py
@@ -23,8 +23,7 @@ def test_missing_extensions(ape_cli, runner, project):
 @skip_projects_except(["unregistered-contracts"])
 def test_no_compiler_for_extension(ape_cli, runner, project):
     result = runner.invoke(ape_cli, ["compile", "contracts/Contract.test"])
-    # TODO: FIX
-    # assert result.exit_code == 0
+    assert result.exit_code == 0
     assert (
         "WARNING: No compilers detected for the " "following extensions: .test"
     ) in result.output
@@ -38,8 +37,7 @@ def test_compile(ape_cli, runner, project):
     for file in project.path.glob("contracts/**/*"):
         assert file.stem in result.output
     result = runner.invoke(ape_cli, ["compile"])
-    # TODO: FIX
-    # assert result.exit_code == 0
+    assert result.exit_code == 0
     # First time it compiles, it caches
     for file in project.path.glob("contracts/**/*"):
         assert file.stem not in result.output
