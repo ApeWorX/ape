e493ee12f1db2d4a436adf998efa4a18a265a1e5
unparalleled-js <yingthi@live.com>
2022-01-18 22:19:02 -0600

    docs: mkdir if not exists

--- build_docs.py
+++ build_docs.py
@@ -76,6 +76,7 @@ def main():
             shutil.copytree(build_dir, STABLE_PATH)
 
     # Set up the redirect at /index.html
+    DOCS_BUILD_PATH.mkdir(exist_ok=True, parents=True)
     with open(DOCS_BUILD_PATH / "index.html", "w") as f:
         redirect = "latest" if is_ephemeral else "stable"
         f.write(REDIRECT_HTML.format(redirect))
