13c3abca8f4b5edc994eee13b876e1b9e3a71836
johnson2427 <37009091+johnson2427@users.noreply.github.com>
2022-01-27 19:38:45 -0600

    feat: timestamp converter (#441)
    added convert to timestamp feature. Encompasses strings, datetime objects and timedelta objects

--- tests/functional/conversion/test_timestamp.py
+++ tests/functional/conversion/test_timestamp.py
@@ -1,6 +1,25 @@
+from datetime import datetime, timedelta, timezone
+
+import pytest
+
 from ape import convert
 
 
-def test_convert_string_timestamp():
-    time = "10-15-2021 12:15:12"
-    assert convert(time, int) == 1634300112
+@pytest.mark.parametrize(
+    "args",
+    (
+        [
+            "10-15-2021 12:15:12",
+            "2021-10-15 12:15:12",
+            "15-10-2021 12:15:12",
+            datetime.fromisoformat("2021-10-15 12:15:12"),
+        ]
+    ),
+)
+def test_convert_string_timestamp(args):
+    assert convert(args, int) == 1634300112
+
+
+def test_convert_timedelta_timestamp():
+    delta = timedelta(days=2, hours=2, minutes=12)
+    assert convert(delta, int) == int((datetime.now(timezone.utc) + delta).timestamp())
