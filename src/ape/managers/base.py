c5b1bd8be2c066bd06ac61778b5f25853e79b14d
Evan Blank <32708219+evanblank3@users.noreply.github.com>
2022-01-31 18:30:11 -0500

    docs: ManagerAccessBase

--- src/ape/managers/base.py
+++ src/ape/managers/base.py
@@ -2,4 +2,8 @@ from ape.utils import ManagerAccessBase
 
 
 class ManagerBase(ManagerAccessBase):
+    """
+    NOTE: this is to allow us to add other IPython integration features for managers
+    """
+
     pass
