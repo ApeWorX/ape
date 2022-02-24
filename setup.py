f16196496ec8b9630c3139bd1b75ade97b3a110c
Juliya Smith <yingthi@live.com>
2022-02-24 12:50:21 -0600

    fix: register pytest plugin outside of plugin (#510)

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ setup(
     ],
     entry_points={
         "console_scripts": ["ape=ape._cli:cli"],
-        "pytest11": ["ape_test=ape_test.plugin"],
+        "pytest11": ["ape_test=ape.pytest.plugin"],
         "ape_cli_subcommands": [
             "ape_accounts=ape_accounts._cli:cli",
             "ape_compile=ape_compile._cli:cli",
