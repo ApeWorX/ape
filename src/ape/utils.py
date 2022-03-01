188dc8680f3d4568d9ad8f1382b844d740d25f6d
Evan Blank <32708219+evanblank3@users.noreply.github.com>
2022-03-01 12:54:21 -0500

    refactor: remove comments in dict and json

--- src/ape/utils.py
+++ src/ape/utils.py
@@ -632,32 +632,25 @@ class AbstractBaseModel(AbstractBase, BaseModel):
         return [member for member in super().__dir__() if not member.startswith("_")]
 
     def dict(self, *args, **kwargs) -> dict:
-        # NOTE: We do this to accomodate the aliases needed for EIP-2678 compatibility
         if "by_alias" not in kwargs:
             kwargs["by_alias"] = True
 
-        # EIP-2678: skip empty fields (at least by default)
         if "exclude_none" not in kwargs:
             kwargs["exclude_none"] = True
 
         return super().dict(*args, **kwargs)
 
     def json(self, *args, **kwargs) -> str:
-        # NOTE: When serializing to IPFS, the canonical representation must be repeatable
 
-        # EIP-2678: minified representation (at least by default)
         if "separators" not in kwargs:
             kwargs["separators"] = (",", ":")
 
-        # EIP-2678: sort keys (at least by default)
         if "sort_keys" not in kwargs:
             kwargs["sort_keys"] = True
 
-        # NOTE: We do this to accomodate the aliases needed for EIP-2678 compatibility
         if "by_alias" not in kwargs:
             kwargs["by_alias"] = True
 
-        # EIP-2678: skip empty fields (at least by default)
         if "exclude_none" not in kwargs:
             kwargs["exclude_none"] = True
 
