665b1ef8c765d3d364019a11c12c89d9bc49ed12
Just some guy <3859395+fubuloubu@users.noreply.github.com>
2022-02-28 23:16:31 -0500

    test: add regression test about re-imports inside scripts folder

---
+++ tests/integration/cli/projects/script/scripts/_utils.py
@@ -0,0 +1,2 @@
+# Note that any file prepended w/ underscore is ignored
+deploy = lambda: None  # noqa: E731
--- tests/integration/cli/projects/script/scripts/deploy.py
+++ tests/integration/cli/projects/script/scripts/deploy.py
@@ -1,5 +1,8 @@
 from ape import networks
 
+# Make sure the re-import bug goes away
+from .utils import deploy  # noqa: F401
+
 
 def main():
     assert networks.active_provider.name == "test"
