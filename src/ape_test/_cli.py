3f4c70ee707827ef0dc2c9bb879e2c8d102f0806
Juliya Smith <yingthi@live.com>
2022-03-05 15:25:41 -0600

    fix: handle exiting pytest when provider never connects (#545)

--- src/ape_test/_cli.py
+++ src/ape_test/_cli.py
@@ -17,5 +17,4 @@ def cli(cli_ctx, pytest_args):
     return_code = pytest.main([*pytest_args], ["ape_test"])
     if return_code:
         # only exit with non-zero status to make testing easier
-        cli_ctx.logger.error("Failed to run pytest")
         sys.exit(return_code)
