e80b6d0dd755e34f08b65407bbe64b89f1a3d409
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2022-03-09 01:10:24 -0500

    refactor: do script loading more dynamically (#522)
    * refactor: do script loading more dynamically
    
    * refactor: don't throw if no scripts folder or scripts detected
    
    * refactor: display relative path inside help for `ape run`
    
    * test: refactor script run testing
    
    * refactor: no need for making scripts a module
    
    * refactor: ensure tests run correctly
    
    * test: add regression test about re-imports inside scripts folder
    
    * fix: wrong path
    
    * fix: try adding `__init__.py`
    
    * refactor: clean up and try a different method for loading scripts
    
    * refactor: don't use a lambda
    
    Co-authored-by: Juliya Smith <yingthi@live.com>
    
    * fix: refactored `config` to `config_manager`
    
    * refactor: add a temporary sys path context manager
    
    * refactor: use `runpy.run_module` for executing scripts
    
    * fix: scripts now no longer use networking by default
    
    * refactor: handle bug in loading click cli
    
    also, remove unnecessary checks on loading script namespace
    
    * refactor: add `--network` option to non-cli scripts
    
    * docs: add docs for `ape run`
    
    * fix: stranded return statement
    
    * refactor: no longer need to have with statement in `main` statements
    
    Co-authored-by: Juliya Smith <yingthi@live.com>

--- tests/integration/cli/test_run.py
+++ tests/integration/cli/test_run.py
@@ -1,36 +1,27 @@
-from .utils import skip_projects, skip_projects_except
+from .utils import skip_projects_except
 
 BAD_COMMAND = "not-a-name"
-
-
-@skip_projects(["script", "geth"])
-def test_run_no_scripts_dir(ape_cli, runner, project):
-    result = runner.invoke(ape_cli, ["run", BAD_COMMAND])
-    assert result.exit_code == 1, result.output
-    assert "No 'scripts/' directory detected to run script" in result.output
-
-
-@skip_projects_except(["script"])
-def test_run_no_argument(ape_cli, runner, project):
-    result = runner.invoke(ape_cli, ["run"])
-    assert result.exit_code == 1, result.output
-    assert "Must provide at least one script name or path" in result.output
 
 
 @skip_projects_except(["script"])
 def test_run_unknown_script(ape_cli, runner, project):
     result = runner.invoke(ape_cli, ["run", BAD_COMMAND])
-    assert result.exit_code == 1
-    assert f"No script named '{BAD_COMMAND}' detected in scripts folder" in result.output
+    assert result.exit_code == 2
+    assert f"No such command '{BAD_COMMAND}'." in result.output
 
 
-@skip_projects(
-    ["empty-config", "no-config", "one-interface", "unregistered-contracts", "test", "geth"]
-)
 def test_run(ape_cli, runner, project):
-    for script_file in project.scripts_folder.glob("*.py"):
-        if script_file.stem == "__init__":
-            continue
+    result = runner.invoke(ape_cli, ["run"])
+    assert result.exit_code == 0, result.output
+    # By default, no commands are run
+    assert "Super secret script output" not in result.output
 
+    for script_file in project.scripts_folder.glob("*.py"):
         result = runner.invoke(ape_cli, ["run", script_file.stem])
         assert result.exit_code == 0, result.output
+
+        if script_file.stem.startswith("_"):
+            assert "Super secret script output" not in result.output
+
+        else:
+            assert "Super secret script output" in result.output
