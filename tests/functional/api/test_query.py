1b22a4a71a80870864480585480a55b9c7679915
Blake Johnson <blakeejohnson39@gmail.com>
2022-03-10 08:43:38 -0600

    chore: cleaned up query test

--- tests/functional/api/test_query.py
+++ tests/functional/api/test_query.py
@@ -1,11 +1,8 @@
-from ape import accounts, chain
+from ape import chain
 
 
 def test_basic_query(test_provider):
-    a = accounts.test_accounts[0]
-    a.transfer(a, 100)
-    a.transfer(a, 100)
-    a.transfer(a, 100)
+    chain.mine(3)
     assert chain.blocks.query("number").to_dict() == {"number": {0: 0, 1: 1, 2: 2}}
     df = chain.blocks.query("number", "timestamp")
     assert len(df) == 3
