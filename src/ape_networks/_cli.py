4031cca0ccb38dc546acf77d024fff4ae774d61f
unparalleled-js <yingthi@live.com>
2022-03-10 17:34:56 -0600

    feat: allow filitering on cli

--- src/ape_networks/_cli.py
+++ src/ape_networks/_cli.py
@@ -19,7 +19,12 @@ def cli():
 @cli.command(name="list", short_help="List registered networks")
 @ape_cli_context()
 @output_format_option()
-def _list(cli_ctx, output_format):
+@click.option(
+    "--ecosystem", "ecosystem_filter", multiple=Tree, help="Filter the results by ecosystem"
+)
+@click.option("--network", "network_filter", multiple=Tree, help="Filter the results by ecosystem")
+@click.option("--provider", "provider_filter", multiple=Tree, help="Filter the results by provider")
+def _list(cli_ctx, output_format, ecosystem_filter, network_filter, provider_filter):
     if output_format == OutputFormat.TREE:
         default_suffix = "[dim default]  (default)"
         ecosystems = cli_ctx.network_manager.network_data["ecosystems"]
@@ -33,13 +38,22 @@ def _list(cli_ctx, output_format):
             return sub_tree
 
         for ecosystem in ecosystems:
+            if ecosystem_filter and ecosystem["name"] not in ecosystem_filter:
+                continue
+
             ecosystem_tree = make_sub_tree(ecosystem, Tree)
             _networks = ecosystem["networks"]
             for network in _networks:
+                if network_filter and network["name"] not in network_filter:
+                    continue
+
                 providers = network["providers"]
                 if providers:
                     network_tree = make_sub_tree(network, ecosystem_tree.add)
                     for provider in providers:
+                        if provider_filter and provider["name"] not in provider_filter:
+                            continue
+
                         make_sub_tree(provider, network_tree.add)
 
             if _networks:
