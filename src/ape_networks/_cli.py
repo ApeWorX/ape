f0d5a43170c004658887af33299eda600cf73870
unparalleled-js <yingthi@live.com>
2022-03-10 18:12:12 -0600

    feat: smarter filtering and validation on cli

--- src/ape_networks/_cli.py
+++ src/ape_networks/_cli.py
@@ -4,9 +4,44 @@ import click
 from rich import print as echo_rich_text
 from rich.tree import Tree
 
+from ape import networks
 from ape.cli import ape_cli_context
 from ape.cli.choices import OutputFormat
 from ape.cli.options import output_format_option
+
+
+def _filter_option(name: str, options):
+    return click.option(
+        f"--{name}",
+        f"{name}_filter",
+        multiple=Tree,
+        help=f"Filter the results by {name}",
+        type=click.Choice(options),
+    )
+
+
+def ecosystem_filter_option():
+    options = [e for e in networks.ecosystems]
+    return _filter_option("ecosystem", options)
+
+
+def network_filter_option():
+    options = set()
+    for ecosystem in networks.ecosystems.values():
+        for network in ecosystem.networks:
+            options.add(network)
+
+    return _filter_option("network", options)
+
+
+def provider_filter_option():
+    options = set()
+    for ecosystem in networks.ecosystems.values():
+        for network in ecosystem.networks.values():
+            for provider in network.providers:
+                options.add(provider)
+
+    return _filter_option("provider", options)
 
 
 @click.group(short_help="Manage networks")
@@ -19,11 +54,9 @@ def cli():
 @cli.command(name="list", short_help="List registered networks")
 @ape_cli_context()
 @output_format_option()
-@click.option(
-    "--ecosystem", "ecosystem_filter", multiple=Tree, help="Filter the results by ecosystem"
-)
-@click.option("--network", "network_filter", multiple=Tree, help="Filter the results by ecosystem")
-@click.option("--provider", "provider_filter", multiple=Tree, help="Filter the results by provider")
+@ecosystem_filter_option()
+@network_filter_option()
+@provider_filter_option()
 def _list(cli_ctx, output_format, ecosystem_filter, network_filter, provider_filter):
     if output_format == OutputFormat.TREE:
         default_suffix = "[dim default]  (default)"
@@ -43,20 +76,24 @@ def _list(cli_ctx, output_format, ecosystem_filter, network_filter, provider_fil
 
             ecosystem_tree = make_sub_tree(ecosystem, Tree)
             _networks = ecosystem["networks"]
+            if network_filter:
+                _networks = [n for n in _networks if n["name"] in network_filter]
+
             for network in _networks:
                 if network_filter and network["name"] not in network_filter:
                     continue
 
                 providers = network["providers"]
+                if provider_filter:
+                    providers = [p for p in providers if p["name"] in provider_filter]
+
                 if providers:
                     network_tree = make_sub_tree(network, ecosystem_tree.add)
                     for provider in providers:
-                        if provider_filter and provider["name"] not in provider_filter:
-                            continue
-
                         make_sub_tree(provider, network_tree.add)
 
-            if _networks:
+            if _networks and ecosystem_tree.children:
                 echo_rich_text(ecosystem_tree)
+
     elif output_format == OutputFormat.YAML:
         click.echo(cli_ctx.network_manager.networks_yaml.strip())
