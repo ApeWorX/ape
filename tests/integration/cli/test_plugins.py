b0b35cd36f20985b50c7fdbee671339723aaee8b
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2022-03-23 13:29:29 -0700

    feat: add more core plugin assert

--- tests/integration/cli/test_plugins.py
+++ tests/integration/cli/test_plugins.py
@@ -3,3 +3,5 @@ def test_list_excludes_core_plugins(ape_cli, runner):
     assert result.exit_code == 0, result.output
     assert "console" not in result.output, "console is not supposed to be in Installed Plugins"
     assert "networks" not in result.output, "networks is not supposed to be in Installed Plugins"
+    assert "geth" not in result.output, "networks is not supposed to be in Installed Plugins"
+    assert "init" not in result.output, "networks is not supposed to be in Installed Plugins"
