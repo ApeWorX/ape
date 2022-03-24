15d78123d2a899ab67709b50233e40d5681383a9
Blake Johnson <blakeejohnson39@gmail.com>
2022-03-23 22:16:32 -0500

    chore: returned back to what I had, still in review. Previous commit was completely wrong

--- src/ape/managers/chain.py
+++ src/ape/managers/chain.py
@@ -134,8 +134,10 @@ class BlockContainer(BaseManager):
 
         return self.query_manager.query(query)
 
-    def range(self, start_or_stop: int, step: int = 1) -> Iterator[BlockAPI]:
-        return self.query_manager.get_blocks(start_or_stop, stop=None, step=step)
+    def range(
+        self, start_or_stop: int, stop: Optional[int] = None, step: int = 1
+    ) -> Iterator[BlockAPI]:
+        return self.query_manager.get_blocks(start_or_stop, stop, step)
 
     def poll_blocks(
         self,
