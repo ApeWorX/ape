9014e9ad43c59f3c79806524a2c9e563c4981fe0
Blake Johnson <blakeejohnson39@gmail.com>
2022-03-30 10:27:09 -0500

    refactor: added 2 tests and another assert to test_basic_query

--- tests/functional/api/test_query.py
+++ tests/functional/api/test_query.py
@@ -1,4 +1,8 @@
+import pytest
+from pydantic import ValidationError
+
 from ape import chain
+from ape.api.query import AccountQuery, BlockQuery, ContractEventQuery, ContractMethodQuery
 
 
 def test_basic_query(eth_tester_provider):
@@ -7,3 +11,28 @@ def test_basic_query(eth_tester_provider):
     df = chain.blocks.query("number", "timestamp")
     assert len(df) == 4
     assert df["timestamp"][3] > df["timestamp"][2] >= df["timestamp"][1] >= df["timestamp"][0]
+    df_all = chain.blocks.query("*")
+    columns = list(df_all.columns)
+    assert [
+        "gas_data",
+        "consensus_data",
+        "hash",
+        "number",
+        "parent_hash",
+        "size",
+        "timestamp",
+    ] == columns
+
+
+def test_block_query(eth_tester_provider):
+    chain.mine(3)
+    with pytest.raises(ValidationError) as err:
+        BlockQuery(columns=["columns"], start_block=0, stop_block=2)
+    assert "Unrecognized field 'columns" in str(err.value)
+
+
+def test_account_query(eth_tester_provider):
+    chain.mine(3)
+    with pytest.raises(ValidationError) as err:
+        AccountQuery(columns=["colu"], start_nonce=0, stop_nonce=2)
+    assert "validation error for AccountQuery" in str(err.value)
