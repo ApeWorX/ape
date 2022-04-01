7b84ee67f97db364aec545d5fdcc9aeda3736faa
unparalleled-js <yingthi@live.com>
2022-04-01 17:50:02 -0500

    chore: remove unrelated changes

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
         "click>=8.1.0",
         "eth-account==0.5.7",
         # TODO: update when ethpm-types v0.1.1 is released, after ape v0.2.0 release
-        "ethpm-types>=0.1.0b7",
+        "ethpm-types>=0.1.0b7,<0.1.1",
         "evm-trace>=0.1.0.a2",
         "hexbytes>=0.2.2,<1.0.0",
         "packaging>=20.9,<21.0",
