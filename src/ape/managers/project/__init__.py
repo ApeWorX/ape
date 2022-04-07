73222da92a7950282912a64698a82e3184b93663
unparalleled-js <yingthi@live.com>
2022-04-07 08:59:49 -0500

    refactor: separate project modules

---
+++ src/ape/managers/project/__init__.py
@@ -0,0 +1,7 @@
+from ape.managers.project.dependency import DependencyManager
+from ape.managers.project.manager import ProjectManager
+
+__all__ = [
+    "DependencyManager",
+    "ProjectManager",
+]
