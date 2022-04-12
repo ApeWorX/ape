103e5c4d838264a7813fe609bb5e199640d720fc
johnson2427 <blakeejohnson39@gmail.com>
2022-04-12 16:12:19 -0500

    chore: remove comments

--- src/ape_cache/dependencies.py
+++ src/ape_cache/dependencies.py
@@ -4,10 +4,8 @@ from contextlib import contextmanager
 
 @contextmanager
 def get_db():
-    # Code to acquire resource, e.g.:
     db = SessionLocal()
     try:
         yield db
     finally:
-        # Code to release resource, e.g.:
         db.close()
