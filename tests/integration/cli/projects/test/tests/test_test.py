dabd7d2fe5731b4dfd21714d912ce3608fc75952
Edward Amor <edward.amor3@gmail.com>
2022-04-12 20:25:28 +0000

    test: test isolation functions correctly

--- tests/integration/cli/projects/test/tests/test_test.py
+++ tests/integration/cli/projects/test/tests/test_test.py
@@ -1,2 +1,31 @@
+import pytest
+
+
+@pytest.fixture(scope="session")
+def alice(accounts):
+    yield accounts[0]
+
+
+@pytest.fixture(scope="session")
+def bob(accounts):
+    yield accounts[1]
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup(alice, bob):
+    alice.transfer(bob, 10**18)
+
+
 def test_test():
     assert True
+
+
+def test_isolation_first(alice, bob, chain):
+    assert chain.provider.get_block("latest").number == 1
+    assert bob.balance == 1_000_001 * 10**18
+    alice.transfer(bob, "1 ether")
+
+
+def test_isolation_second(bob, chain):
+    assert chain.provider.get_block("latest").number == 1
+    assert bob.balance == 1_000_001 * 10**18
