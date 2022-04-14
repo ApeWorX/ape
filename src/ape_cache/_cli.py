9981c8f5c0acfe3f955cd6f5d8215b63986916a6
johnson2427 <blakeejohnson39@gmail.com>
2022-04-14 09:20:10 -0500

    chore: black linting fix

--- src/ape_cache/_cli.py
+++ src/ape_cache/_cli.py
@@ -13,9 +13,11 @@ def cli():
     Command-line helper for managing query caching database.
     """
 
+
 @cli.command(short_help="Initialize a new cache db")
 def init():
     models.Base.metadata.create_all(bind=engine)
+
 
 @cli.command(short_help="Call and print SQL Statement to the cache db")
 @click.argument("sql")
@@ -23,6 +25,7 @@ def query(sql):
     with get_db() as db:
         click.echo(pd.DataFrame(db.execute(sql)))
 
+
 @cli.command(short_help="")
-def migrate(db: Session = get_db):
+def migrate():
     pass
