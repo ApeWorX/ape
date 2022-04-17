bc1925084cb55be50d1edd1cb3b11d4b66d675a8
Edward Amor <edward.amor3@gmail.com>
2022-04-17 10:15:43 +0000

    fix: warn on snapshot not implemented in isolation fixtures

--- src/ape/pytest/fixtures.py
+++ src/ape/pytest/fixtures.py
@@ -3,12 +3,16 @@ from typing import Iterator, List
 import pytest
 
 from ape.api import TestAccountAPI
+from ape.logging import logger
 from ape.managers.chain import ChainManager
 from ape.managers.project import ProjectManager
 from ape.utils import ManagerAccessMixin
 
 
 class PytestApeFixtures(ManagerAccessMixin):
+    def __init__(self):
+        self._warned_for_missing_features = False
+
     @pytest.fixture(scope="session")
     def accounts(self) -> List[TestAccountAPI]:
         return self.account_manager.test_accounts
@@ -23,35 +27,75 @@ class PytestApeFixtures(ManagerAccessMixin):
 
     @pytest.fixture(scope="session")
     def _session_isolation(self) -> Iterator[None]:
-        snapshot_id = self.chain_manager.snapshot()
+        snapshot_id = None
+        try:
+            snapshot_id = self.chain_manager.snapshot()
+        except NotImplementedError:
+            self._warn_for_unimplemented_snapshot()
+
         yield
-        if snapshot_id in self.chain_manager._snapshots:
+
+        if snapshot_id is not None and snapshot_id in self.chain_manager._snapshots:
             self.chain_manager.restore(snapshot_id)
 
     @pytest.fixture(scope="package")
     def _package_isolation(self, _session_isolation) -> Iterator[None]:
-        snapshot_id = self.chain_manager.snapshot()
+        snapshot_id = None
+        try:
+            snapshot_id = self.chain_manager.snapshot()
+        except NotImplementedError:
+            self._warn_for_unimplemented_snapshot()
+
         yield
-        if snapshot_id in self.chain_manager._snapshots:
+
+        if snapshot_id is not None and snapshot_id in self.chain_manager._snapshots:
             self.chain_manager.restore(snapshot_id)
 
     @pytest.fixture(scope="module")
     def _module_isolation(self, _package_isolation) -> Iterator[None]:
-        snapshot_id = self.chain_manager.snapshot()
+        snapshot_id = None
+        try:
+            snapshot_id = self.chain_manager.snapshot()
+        except NotImplementedError:
+            self._warn_for_unimplemented_snapshot()
+
         yield
-        if snapshot_id in self.chain_manager._snapshots:
+
+        if snapshot_id is not None and snapshot_id in self.chain_manager._snapshots:
             self.chain_manager.restore(snapshot_id)
 
     @pytest.fixture(scope="class")
     def _class_isolation(self, _module_isolation) -> Iterator[None]:
-        snapshot_id = self.chain_manager.snapshot()
+        snapshot_id = None
+        try:
+            snapshot_id = self.chain_manager.snapshot()
+        except NotImplementedError:
+            self._warn_for_unimplemented_snapshot()
+
         yield
-        if snapshot_id in self.chain_manager._snapshots:
+
+        if snapshot_id is not None and snapshot_id in self.chain_manager._snapshots:
             self.chain_manager.restore(snapshot_id)
 
     @pytest.fixture(scope="function")
     def _function_isolation(self, _class_isolation) -> Iterator[None]:
-        snapshot_id = self.chain_manager.snapshot()
+        snapshot_id = None
+        try:
+            snapshot_id = self.chain_manager.snapshot()
+        except NotImplementedError:
+            self._warn_for_unimplemented_snapshot()
+
         yield
-        if snapshot_id in self.chain_manager._snapshots:
+
+        if snapshot_id is not None and snapshot_id in self.chain_manager._snapshots:
             self.chain_manager.restore(snapshot_id)
+
+    def _warn_for_unimplemented_snapshot(self):
+        if self._warned_for_missing_features:
+            return
+
+        logger.warning(
+            "The connected provider does not support snapshotting. "
+            "Tests will not be completely isolated."
+        )
+        self._warned_for_missing_features = True
