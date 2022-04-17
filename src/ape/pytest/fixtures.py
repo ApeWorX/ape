4c64d1eca911b8abbdd50f24cc1bc44e9f7a8488
Edward Amor <edward.amor3@gmail.com>
2022-04-17 09:28:34 +0000

    feat: add internal scope isolation fixtures

--- src/ape/pytest/fixtures.py
+++ src/ape/pytest/fixtures.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Iterator, List
 
 import pytest
 
@@ -20,3 +20,38 @@ class PytestApeFixtures(ManagerAccessMixin):
     @pytest.fixture(scope="session")
     def project(self) -> ProjectManager:
         return self.project_manager
+
+    @pytest.fixture(scope="session")
+    def _session_isolation(self) -> Iterator[None]:
+        snapshot_id = self.chain_manager.snapshot()
+        yield
+        if snapshot_id in self.chain_manager._snapshots:
+            self.chain_manager.restore(snapshot_id)
+
+    @pytest.fixture(scope="package")
+    def _package_isolation(self, _session_isolation) -> Iterator[None]:
+        snapshot_id = self.chain_manager.snapshot()
+        yield
+        if snapshot_id in self.chain_manager._snapshots:
+            self.chain_manager.restore(snapshot_id)
+
+    @pytest.fixture(scope="module")
+    def _module_isolation(self, _package_isolation) -> Iterator[None]:
+        snapshot_id = self.chain_manager.snapshot()
+        yield
+        if snapshot_id in self.chain_manager._snapshots:
+            self.chain_manager.restore(snapshot_id)
+
+    @pytest.fixture(scope="class")
+    def _class_isolation(self, _module_isolation) -> Iterator[None]:
+        snapshot_id = self.chain_manager.snapshot()
+        yield
+        if snapshot_id in self.chain_manager._snapshots:
+            self.chain_manager.restore(snapshot_id)
+
+    @pytest.fixture(scope="function")
+    def _function_isolation(self, _class_isolation) -> Iterator[None]:
+        snapshot_id = self.chain_manager.snapshot()
+        yield
+        if snapshot_id in self.chain_manager._snapshots:
+            self.chain_manager.restore(snapshot_id)
