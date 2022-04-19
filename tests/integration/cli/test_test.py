2bf05996165b93a23d474ba3aaed017a1a4ef892
Edward Amor <edward.amor3@gmail.com>
2022-04-19 04:52:22 +0000

    test: disable isolation flag

--- tests/integration/cli/test_test.py
+++ tests/integration/cli/test_test.py
@@ -3,5 +3,14 @@ from .utils import skip_projects_except
 
 @skip_projects_except(["test"])
 def test_test(ape_cli, runner):
+    # test cases implicitly test built-in isolation
     result = runner.invoke(ape_cli, ["test"])
     assert result.exit_code == 0, result.output
+
+
+@skip_projects_except(["test"])
+def test_test_isolation_disabled(ape_cli, runner):
+    # check the disable isolation option actually disables built-in isolation
+    # also don't run the tests just show what fixtures/tests would be run
+    result = runner.invoke(ape_cli, ["test", "--disable-isolation", "--setup-plan"])
+    assert "F _function_isolation" not in result.output
