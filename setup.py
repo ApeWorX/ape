856d9734468e6b06d05e72e7d912a1819e37009a
unparalleled-js <yingthi@live.com>
2022-05-20 10:54:52 -0500

    chore: update flake8

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ extras_require = {
         "mypy>=0.950,<1.0",  # Static type analyzer
         "types-PyYAML",  # NOTE: Needed due to mypy typeshed
         "types-requests",  # NOTE: Needed due to mypy typeshed
-        "flake8>=3.9.2,<4.0",  # Style linter
+        "flake8>=4.0.1,<5.0",  # Style linter
         "flake8-breakpoint>=1.1.0,<2.0.0",  # detect breakpoints left in code
         "flake8-print>=4.0.0,<5.0.0",  # detect print statements left in code
         "isort>=5.10.1,<6.0",  # Import sorting linter
