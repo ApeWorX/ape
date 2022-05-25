c5dccd9bb648e5635116c622d86d34cf97fbb408
johnson2427 <blakeejohnson39@gmail.com>
2022-05-24 19:11:58 -0500

    fix: functionality of map function is the same, removed lambda

--- src/ape/api/providers.py
+++ src/ape/api/providers.py
@@ -93,7 +93,7 @@ class BlockAPI(BaseInterfaceModel):
     def transactions(self) -> List[TransactionAPI]:
         query = BlockTransactionQuery(block_id=self.hash)
         df_txns = self.query_manager.query(query)
-        return list(map(lambda x: TransactionAPI(x), df_txns.values.tolist()))
+        return list(map(TransactionAPI, df_txns.values.tolist()))
 
 
 class ProviderAPI(BaseInterfaceModel):
