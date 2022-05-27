891ff50e26fe1ef806ae4e49109e5bc6e5fa559e
unparalleled-js <yingthi@live.com>
2022-05-26 20:44:02 -0500

    test: fix tests

--- tests/integration/cli/test_project.py
+++ tests/integration/cli/test_project.py
@@ -1,14 +1,14 @@
 from .utils import skip_projects_except
 
 
-@skip_projects_except(["with-dependency"])
+@skip_projects_except(["with-dependencies"])
 def test_get_project_without_contracts_path(project):
     proj_path = project.path / "dependency_c"
     proj = project.get_project(proj_path)
     assert proj.contracts_folder == proj_path / "contracts"
 
 
-@skip_projects_except(["with-dependency"])
+@skip_projects_except(["with-dependencies"])
 def test_get_project_with_contracts_path(project):
     proj_path = project.path / "dependency_a"
     proj = project.get_project(proj_path, proj_path / "sources")
