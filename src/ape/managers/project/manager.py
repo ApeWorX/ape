099d610cf73b8854c709a6d197292643d7e3b3d2
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2022-06-13 15:32:16 -0700

    refactor: commented out publish_manifest method

--- src/ape/managers/project/manager.py
+++ src/ape/managers/project/manager.py
@@ -455,12 +455,12 @@ class ProjectManager(BaseManager):
         except Exception as e:
             raise ConfigError(f"Incorrect configuration of package metadata:\n{meta}") from e
 
-    def publish_manifest(self):
-        manifest = self.manifest.dict()
-        if not manifest["name"]:
-            raise ProjectError("Need name to release manifest")
-        if not manifest["version"]:
-            raise ProjectError("Need version to release manifest")
+    # def publish_manifest(self):
+    #     manifest = self.manifest.dict()
+    #     if not manifest["name"]:
+    #         raise ProjectError("Need name to release manifest")
+    #     if not manifest["version"]:
+    #         raise ProjectError("Need version to release manifest")
 
     #     TODO: Publish sources to IPFS and replace with CIDs
     #     TODO: Publish to IPFS
