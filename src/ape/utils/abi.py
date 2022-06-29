b866bfbb6ec752944bbb5a0c1d2469e8c5d8f9de
banteg <4562643+banteg@users.noreply.github.com>
2022-06-29 14:38:41 +0400

    fix: decode string type for events

--- src/ape/utils/abi.py
+++ src/ape/utils/abi.py
@@ -1,8 +1,7 @@
 import re
 from dataclasses import make_dataclass
-from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
-from eth_abi import grammar
 from eth_utils.abi import collapse_if_tuple
 from ethpm_types.abi import ABIType, EventABI, EventABIType, MethodABI
 
@@ -242,16 +241,7 @@ class LogInputABICollection:
 
     @property
     def types(self) -> List[Union[str, Dict]]:
-        return [t for t in _get_event_abi_types(self.normalized_values)]
-
-
-def _get_event_abi_types(abi_inputs: List[Dict]) -> Iterator[Union[str, Dict]]:
-    for abi_input in abi_inputs:
-        abi_type = grammar.parse(abi_input["type"])
-        if abi_type.is_dynamic:
-            yield "bytes32"
-        else:
-            yield collapse_if_tuple(abi_input)
+        return [collapse_if_tuple(t) for t in self.normalized_values]
 
 
 def parse_type(output_type: str) -> Union[str, Tuple, List]:
