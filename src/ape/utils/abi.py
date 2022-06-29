7d60352215ab4224e7c801e3e7caa5bb835c7dbc
unparalleled-js <yingthi@live.com>
2022-06-29 10:47:40 -0500

    fix: still handle dynamic as bytes when indexed

--- src/ape/utils/abi.py
+++ src/ape/utils/abi.py
@@ -1,11 +1,12 @@
 import re
 from dataclasses import make_dataclass
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
 
+from eth_abi import grammar
 from eth_utils.abi import collapse_if_tuple
 from ethpm_types.abi import ABIType, EventABI, EventABIType, MethodABI
 
-ARRAY_PATTERN = re.compile(r"[\(*\w,? ?\w?\)?]*\[\d?\]")
+ARRAY_PATTERN = re.compile(r"[(*\w,? )]*\[\d?]")
 
 
 def is_array(abi_type: Union[str, ABIType]) -> bool:
@@ -227,9 +228,10 @@ def create_struct(
 
 
 class LogInputABICollection:
-    def __init__(self, abi: EventABI, values: List[EventABIType]):
+    def __init__(self, abi: EventABI, values: List[EventABIType], indexed: bool = False):
         self.abi = abi
         self.values = values
+        self.indexed = indexed
 
     @property
     def names(self) -> List[str]:
@@ -241,7 +243,28 @@ class LogInputABICollection:
 
     @property
     def types(self) -> List[Union[str, Dict]]:
-        return [collapse_if_tuple(t) for t in self.normalized_values]
+        if not self.indexed:
+            return [collapse_if_tuple(t) for t in self.normalized_values]
+
+        # Indexed inputs
+        handled_values: List[Union[str, Dict]] = []
+        for abi_input in self.normalized_values:
+            abi_type = grammar.parse(abi_input["type"])
+            if abi_type.is_dynamic:
+                handled_values.append("bytes32")
+            else:
+                handled_values.append(collapse_if_tuple(abi_input))
+
+        return handled_values
+
+
+def _get_event_abi_types(abi_inputs: List[Dict]) -> Iterator[Union[str, Dict]]:
+    for abi_input in abi_inputs:
+        abi_type = grammar.parse(abi_input["type"])
+        if abi_type.is_dynamic:
+            yield "bytes32"
+        else:
+            yield collapse_if_tuple(abi_input)
 
 
 def parse_type(output_type: str) -> Union[str, Tuple, List]:
