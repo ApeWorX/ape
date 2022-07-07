d876f3d692bc83e91329293e737f245b8cc22dd5
unparalleled-js <yingthi@live.com>
2022-07-07 17:31:58 -0500

    test: add simple tests

--- tests/functional/test_receipt.py
+++ tests/functional/test_receipt.py
@@ -2,8 +2,8 @@ import pytest
 
 
 @pytest.fixture
-def invoke_receipt(solidity_contract_instance, owner):
-    return solidity_contract_instance.setNumber(1, sender=owner)
+def invoke_receipt(vyper_contract_instance, owner):
+    return vyper_contract_instance.setNumber(1, sender=owner)
 
 
 def test_show_trace(invoke_receipt):
@@ -11,3 +11,23 @@ def test_show_trace(invoke_receipt):
     # such as ape-hardhat.
     with pytest.raises(NotImplementedError):
         invoke_receipt.show_trace()
+
+
+def test_decode_logs(invoke_receipt):
+    logs = [log for log in invoke_receipt.decode_logs()]
+    assert len(logs) == 1
+    assert logs[0].newNum == 1
+
+
+def test_decode_logs_specify_abi(invoke_receipt, vyper_contract_instance):
+    abi = vyper_contract_instance.NumberChange.abi
+    logs = [log for log in invoke_receipt.decode_logs(abi=abi)]
+    assert len(logs) == 1
+    assert logs[0].newNum == 1
+
+
+def test_decode_logs_specify_abi_as_event(invoke_receipt, vyper_contract_instance):
+    abi = vyper_contract_instance.NumberChange
+    logs = [log for log in invoke_receipt.decode_logs(abi=abi)]
+    assert len(logs) == 1
+    assert logs[0].newNum == 1
