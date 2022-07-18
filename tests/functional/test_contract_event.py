8b4532de5e64ce8b9d3c9fe1ae8a1f772b52e379
unparalleled-js <yingthi@live.com>
2022-07-18 10:41:08 -0500

    test: correct tests

--- tests/functional/test_contract_event.py
+++ tests/functional/test_contract_event.py
@@ -8,7 +8,7 @@ from ethpm_types import ContractType
 from hexbytes import HexBytes
 
 from ape.api import ReceiptAPI
-from ape.exceptions import ChainError, DecodingError
+from ape.exceptions import ChainError
 from ape.types import ContractLog
 
 
@@ -103,22 +103,20 @@ def test_contract_logs_splicing(contract_instance, owner, assert_log_values):
 
 def test_contract_logs_range(contract_instance, owner, assert_log_values):
     contract_instance.setNumber(1, sender=owner)
-    logs = [
-        log for log in contract_instance.NumberChange.range(100, event_parameters={"newNum": 1})
-    ]
+    logs = [log for log in contract_instance.NumberChange.range(100, search_topics={"newNum": 1})]
     assert len(logs) == 1, "Unexpected number of logs"
     assert_log_values(logs[0], 1)
 
 
 def test_contract_logs_range_by_address(
-    mocker, eth_tester_provider, test_accounts, contract_instance, owner, assert_log_values
+    mocker, chain, eth_tester_provider, test_accounts, contract_instance, owner, assert_log_values
 ):
     spy = mocker.spy(eth_tester_provider.web3.eth, "get_logs")
     contract_instance.setAddress(test_accounts[1], sender=owner)
     logs = [
         log
         for log in contract_instance.AddressChange.range(
-            100, event_parameters={"newAddress": test_accounts[1]}
+            100, search_topics={"newAddress": test_accounts[1]}
         )
     ]
 
@@ -128,8 +126,8 @@ def test_contract_logs_range_by_address(
     spy.assert_called_once_with(
         {
             "address": [contract_instance.address],
-            "fromBlock": 0,
-            "toBlock": 3,
+            "fromBlock": "0x0",
+            "toBlock": f"0x{chain.blocks.height}",
             "topics": [
                 "0x7ff7bacc6cd661809ed1ddce28d4ad2c5b37779b61b9e3235f8262be529101a9",
                 "0x000000000000000000000000c89d42189f0450c2b2c3c61f58ec5d628176a1e7",
@@ -150,7 +148,7 @@ def test_contracts_log_multiple_addresses(
     logs = [
         log
         for log in contract_instance.NumberChange.range(
-            100, event_parameters={"newNum": 1}, extra_addresses=[another_instance.address]
+            100, search_topics={"newNum": 1}, extra_addresses=[another_instance.address]
         )
     ]
     assert len(logs) == 2, "Unexpected number of logs"
@@ -182,44 +180,6 @@ def test_contract_logs_range_only_stop(contract_instance, owner, chain):
     stop = 100  # Stop can be bigger than height, it doesn't not matter
     logs = [log for log in contract_instance.NumberChange.range(stop)]
     assert len(logs) == 3, "Unexpected number of logs"
-
-
-def test_contract_logs_range_with_paging(contract_instance, owner, chain, assert_log_values):
-    # Create 1 log each in the first 3 blocks.
-    for i in range(3):
-        contract_instance.setNumber(i + 1, sender=owner)
-
-    # Mine 3 times to ensure we can handle uneventful blocks.
-    for i in range(3):
-        chain.mine()
-
-    # Create one more log after the empty blocks.
-    contract_instance.setNumber(100, sender=owner)
-
-    logs = [log for log in contract_instance.NumberChange.range(100, block_page_size=1)]
-    assert len(logs) == 4, "Unexpected number of logs"
-    assert_log_values(logs[0], 1)
-    assert_log_values(logs[1], 2)
-    assert_log_values(logs[2], 3)
-    assert_log_values(logs[3], 100, previous_number=3)
-
-
-def test_contract_logs_range_over_paging(contract_instance, owner, chain):
-    # Create 1 log each in the first 3 blocks.
-    for i in range(3):
-        contract_instance.setNumber(i + 1, sender=owner)
-
-    # 50 is way more than 3 but it shouldn't matter.
-    logs = [log for log in contract_instance.NumberChange.range(100, block_page_size=50)]
-    assert len(logs) == 3, "Unexpected number of logs"
-
-
-def test_contract_logs_from_non_indexed_range(contract_instance, owner):
-    contract_instance.setNumber(1, sender=owner)
-    with pytest.raises(DecodingError):
-        _ = [
-            log for log in contract_instance.NumberChange.range(0, event_parameters={"prevNum": 1})
-        ]
 
 
 def test_poll_logs_stop_block_not_in_future(
