087b2e35399aabb4394619740d8000605718c1ca
ibis <toni@apeworx.io>
2022-08-08 15:57:15 -0500

    fix: correct test_parse_type() docstring

--- tests/functional/utils/test_abi.py
+++ tests/functional/utils/test_abi.py
@@ -33,7 +33,7 @@ def test_parse_type(s):
     #   - (int[],)
     #   - (int, int,)
     #   - (int, int)
-    #   - (int, int, int,)
+    #   - (int, int, int)
     #   - (int, int, int,)
     #   - ((int, int), int)
     #   - ((int, int[],), int)
