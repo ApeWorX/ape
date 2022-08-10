0b03250ff6cb9a37838f4b0abe0cfed09a5b47b1
banteg <4562643+banteg@users.noreply.github.com>
2022-08-10 21:17:54 +0400

    fix: contract event creating incorrect log filter

--- src/ape/contracts/base.py
+++ src/ape/contracts/base.py
@@ -339,7 +339,7 @@ class ContractEvent(ManagerAccessMixin):
 
     @property
     def log_filter(self):
-        return LogFilter(addresses=[self.contract], events=[self.abi])
+        return LogFilter.from_event(event=self.abi, addresses=[self.contract], start_block=0)
 
     @singledispatchmethod
     def __getitem__(self, value) -> Union[ContractLog, List[ContractLog]]:
