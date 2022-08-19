682bc47dca1d8fd0b0d68276c58859d2ab65703f
unparalleled-js <yingthi@live.com>
2022-08-19 10:07:44 -0500

    test: refactor automate fixture test better

--- tests/integration/cli/test_test.py
+++ tests/integration/cli/test_test.py
@@ -1,5 +1,7 @@
 import pytest
 
+from ape.pytest.fixtures import PytestApeFixtures
+
 from .utils import skip_projects_except
 
 projects_with_tests = skip_projects_except(["test", "with-contracts"])
@@ -27,16 +29,18 @@ def test_test(ape_test_runner, project):
 def test_test_isolation_disabled(ape_test_runner, project):
     # check the disable isolation option actually disables built-in isolation
     result = ape_test_runner.invoke(["--disable-isolation", "--setup-show"])
-    # assert result.exit_code == (1 if project.path.name == "test" else 0)
+    assert result.exit_code == (1 if project.path.name == "test" else 0)
     assert "F _function_isolation" not in result.output
 
 
 @projects_with_tests
 def test_fixture_docs(ape_test_runner, project):
     result = ape_test_runner.invoke(["-q", "--fixtures"])
-    assert "A collection of pre-funded accounts." in result.output
-    assert (
-        "Manipulate the blockchain, such as mine or change the pending timestamp." in result.output
-    )
-    assert "Connect to other networks in your tests." in result.output
-    assert "Access contract types and dependencies." in result.output
+
+    # 'accounts', 'networks', 'chain', and 'project' (etc.)
+    fixtures = [prop for n, prop in vars(PytestApeFixtures).items() if not n.startswith("_")]
+    for fixture in fixtures:
+
+        # The doc str of the fixture shows in the CLI output
+        doc_str = fixture.__doc__.strip()
+        assert doc_str in result.output
