ed1ba6ed16564d1cde7ddbc112fe8975e73c6cc2
unparalleled-js <yingthi@live.com>
2022-08-22 13:10:16 -0500

    test: add more debug info in test test

--- tests/integration/cli/projects/test/tests/test_networks.py
+++ tests/integration/cli/projects/test/tests/test_networks.py
@@ -1,5 +1,19 @@
+import shutil
+
+from ape.exceptions import ProviderNotConnectedError
+
+
 def test_networks_context_does_not_disconnect(networks):
-    assert networks.provider.web3
+    try:
+        assert networks.provider.web3
+    except ProviderNotConnectedError:
+        # Show more info for debugging purposes
+        context = networks.parse_network_choice("ethereum:local:test")
+        error_output = f"""
+        ape: {shutil.which('ape')},
+        connected_providers: {', '.join([pid for pid in context.connected_providers.keys()])},
+        """
+        assert False, f"Provider lost connection!\n{error_output}"
 
     context = networks.ethereum.local.use_provider("test")
 
