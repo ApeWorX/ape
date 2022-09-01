af0e02954ab0a82ca6ecc1b3c0c95c98ed0fa6d0
unparalleled-js <yingthi@live.com>
2022-09-01 16:20:33 -0500

    docs: fix

--- src/ape/api/transactions.py
+++ src/ape/api/transactions.py
@@ -300,8 +300,6 @@ class ReceiptAPI(BaseInterfaceModel):
         """
         Obtain the final return value of the call. Requires tracing to function,
         since this is not available from the receipt object.
-        Returns:
-            Any: The return value (ABI Decoded) of the call
         """
         call_tree = self.provider.get_call_tree(self.txn_hash)
 
