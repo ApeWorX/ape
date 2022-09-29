ab09327df739df1655c74b5fcbd9c739564f8c50
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2022-09-29 13:22:56 -0700

    test: added test for project.extensions_with_missing_compilers()

--- tests/functional/test_compilers.py
+++ tests/functional/test_compilers.py
@@ -1,3 +1,8 @@
 def test_get_imports(project, compilers):
     # See ape-solidity for better tests
     assert not compilers.get_imports(project.source_paths)
+
+
+def test_extensions_with_missing_compilers(project, compilers):
+    result = project.extensions_with_missing_compilers()
+    assert result == []
