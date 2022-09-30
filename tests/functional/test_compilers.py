b836335369df38e9e3c22ec368d724cc006a85da
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2022-09-30 13:35:05 -0700

    test: added test with source files

--- tests/functional/test_compilers.py
+++ tests/functional/test_compilers.py
@@ -3,6 +3,10 @@ def test_get_imports(project, compilers):
     assert not compilers.get_imports(project.source_paths)
 
 
-def test_extensions_with_missing_compilers(project, compilers):
+def test_missing_compilers_without_source_files(project):
     result = project.extensions_with_missing_compilers()
     assert result == []
+
+def test_missing_compilers_with_source_files(project_with_source_files_contract):
+    result = project_with_source_files_contract.extensions_with_missing_compilers()
+    assert result == ['.sol', '.vy']
