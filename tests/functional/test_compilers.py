79899a055b148f77ba1a249b7ff9d4c2abbd3157
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2022-10-03 13:47:41 -0700

    test: added test for missing compilers error message

--- tests/functional/test_compilers.py
+++ tests/functional/test_compilers.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_get_imports(project, compilers):
     # See ape-solidity for better tests
     assert not compilers.get_imports(project.source_paths)
@@ -12,3 +15,12 @@ def test_missing_compilers_with_source_files(project_with_source_files_contract)
     result = project_with_source_files_contract.extensions_with_missing_compilers()
     assert ".vy" in result
     assert ".sol" in result
+
+
+def test_missing_compilers_error_message(project_with_source_files_contract, sender):
+    with pytest.raises(AttributeError) as err:
+        project_with_source_files_contract.ContractA.deploy(sender=sender)
+    assert (
+        err.value.args[0]
+        == "ProjectManager has no attribute or contract named 'ContractA'. Could it be from one of the missing compilers for extensions:.sol, .vy"
+    )
