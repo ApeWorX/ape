e25d0a8d6743e1ae28e2c00fabd37d51e3ad94b0
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2022-11-04 13:36:00 -0700

    chore: linting

--- src/ape/api/providers.py
+++ src/ape/api/providers.py
@@ -43,7 +43,7 @@ from ape.exceptions import (
     VirtualMachineError,
 )
 from ape.logging import logger
-from ape.types import AddressType, BlockID, ContractLog, LogFilter, SnapshotID, ContractCode
+from ape.types import AddressType, BlockID, ContractCode, ContractLog, LogFilter, SnapshotID
 from ape.utils import (
     EMPTY_BYTES32,
     BaseInterfaceModel,
