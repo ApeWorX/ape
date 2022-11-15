ce1243e23e03bf6e6a4f06d1716bbc33bcf1c18b
unparalleled-js <yingthi@live.com>
2022-11-15 15:07:38 -0600

    fix: bump evm trace

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ setup(
         # ** Dependencies maintained by ApeWorX **
         "eip712>=0.1.4,<0.2",
         "ethpm-types>=0.3.11,<0.4",
-        "evm-trace>=0.1.0a10",
+        "evm-trace>=0.1.0a12",
     ],
     entry_points={
         "console_scripts": ["ape=ape._cli:cli"],
