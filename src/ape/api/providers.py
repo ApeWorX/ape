6b0951c7677d3b662545e7f97aa4ca8b251e195d
Gosuto Inzasheru <mail@gosuto.ai>
2022-11-22 10:57:40 +0100

    fix: convert `block_identifier` to hex

--- src/ape/api/providers.py
+++ src/ape/api/providers.py
@@ -926,7 +926,10 @@ class Web3Provider(ProviderAPI, ABC):
             if value is not None and not isinstance(value, str):
                 txn_dict[field] = to_hex(value)
 
-        block_identifier = kwargs.pop("block_identifier", "latest")
+        if "block_identifier" in kwargs:
+            block_identifier = hex(kwargs["block_identifier"])
+        else:
+            block_identifier = "latest"
         arguments = [txn_dict, block_identifier]
         if "state_override" in kwargs:
             arguments.append(kwargs["state_override"])
