0fdc0eee9ee98d33ad5c813bb55e9dd66e5cba53
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2022-11-30 16:56:35 -0800

    refactor: export is not a python reserved keyword

--- src/ape_accounts/_cli.py
+++ src/ape_accounts/_cli.py
@@ -157,10 +157,10 @@ def _import(cli_ctx, alias, import_from_mnemonic, custom_hd_path):
     )
 
 
-@cli.command(name="export", short_help="Unlock an account and export the private key")
+@cli.command(short_help="Unlock an account and export the private key")
 @ape_cli_context()
 @existing_alias_argument()
-def _export(cli_ctx, alias):
+def export(cli_ctx, alias):
     path = _get_container().data_folder.joinpath(f"{alias}.json")
     account = json.loads(path.read_text())
     password = click.prompt("Enter password to decrypt account", hide_input=True)
