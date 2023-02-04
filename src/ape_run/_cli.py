5f221319d347687f7c06e29384047911cc9297d1
unparalleled-js <yingthi@live.com>
2023-02-04 09:37:07 -0600

    fix: works now

--- src/ape_run/_cli.py
+++ src/ape_run/_cli.py
@@ -1,4 +1,6 @@
+import inspect
 import os
+import sys
 import traceback
 from pathlib import Path
 from runpy import run_module
@@ -177,8 +179,30 @@ class ScriptCommand(click.MultiCommand):
         return result
 
     def _launch_console(self):
-        active_locals = {k: v for k, v in globals().items() if not k.startswith("__")}
-        extra_locals = {**self._namespace.get(self._command_called, {}), **active_locals}
+        trace = inspect.trace()
+        trace_frames = [
+            x for x in trace if x.filename.startswith(str(self._project.scripts_folder))
+        ]
+        if not trace_frames:
+            # Error from Ape internals; avoid launching console.
+            sys.exit(1)
+
+        # Use most recently matching frame.
+        frame = trace_frames[-1].frame
+
+        try:
+            globals_dict = {k: v for k, v in frame.f_globals.items() if not k.startswith("__")}
+            extra_locals = {
+                **self._namespace.get(self._command_called, {}),
+                **globals_dict,
+                **frame.f_locals,
+            }
+
+        finally:
+            # Avoid keeping a reference to a frame to avoid reference cycles.
+            if frame:
+                del frame
+
         return console(project=self._project, extra_locals=extra_locals)
 
 
