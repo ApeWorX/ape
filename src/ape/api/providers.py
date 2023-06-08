2d19ecb5c5abf5d7d02e7a0e3a1f5ad42d36ab29
Doggo <27985126+sabotagebeats@users.noreply.github.com>
2023-06-08 10:24:28 -0700

    fix: validation error

--- src/ape/api/providers.py
+++ src/ape/api/providers.py
@@ -806,6 +806,9 @@ class Web3Provider(ProviderAPI, ABC):
         except BlockNotFound as err:
             raise BlockNotFoundError(block_id) from err
 
+        if "baseFeePerGas" in block_data and block_data.get("baseFeePerGas") is None:
+            block_data["baseFeePerGas"] = 0
+
         return self.network.ecosystem.decode_block(block_data)
 
     def get_nonce(self, address: AddressType, **kwargs) -> int:
