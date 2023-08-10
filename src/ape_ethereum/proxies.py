f8a8fe258bc3926a975c90cfd34f2e46da67623f
banteg <4562643+banteg@users.noreply.github.com>
2023-08-10 05:28:25 +0400

    feat: detect clones with immutable args

--- src/ape_ethereum/proxies.py
+++ src/ape_ethereum/proxies.py
@@ -24,6 +24,7 @@ class ProxyType(IntEnum):
     Delegate = 8  # eip-897 delegate proxy
     ZeroAge = 9  # a more-minimal proxy
     SoladyPush0 = 10  # solady push0 minimal proxy
+    ClonesWithImmutableArgs = 11  # https://github.com/wighawag/clones-with-immutable-args/blob/master/src/ClonesWithImmutableArgs.sol
 
 
 class ProxyInfo(ProxyInfoAPI):
