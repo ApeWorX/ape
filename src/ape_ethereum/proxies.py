3e45507c4d5b6c99582322563245e019ee7c6c71
banteg <4562643+banteg@users.noreply.github.com>
2023-08-11 07:37:52 +0400

    feat: better organize proxies and add solady cwia

--- src/ape_ethereum/proxies.py
+++ src/ape_ethereum/proxies.py
@@ -13,19 +13,41 @@ MINIMAL_PROXY_BYTES = (
 
 
 class ProxyType(IntEnum):
+    # https://eips.ethereum.org/EIPS/eip-1167
     Minimal = 0  # eip-1167 minimal proxy contract
+
+    # https://eips.ethereum.org/EIPS/eip-1967
     Standard = 1  # eip-1967 standard proxy storage slots
     Beacon = 2  # eip-1967 beacon proxy
+
+    # https://eips.ethereum.org/EIPS/eip-1822
     UUPS = 3  # # eip-1822 universal upgradeable proxy standard
+
+    # https://github.com/vyperlang/vyper/blob/v0.2.8/vyper/functions/functions.py#L1428
     Vyper = 4  # vyper <0.2.9 create_forwarder_to
+
+    # https://github.com/0xSplits/splits-contracts/blob/main/contracts/libraries/Clones.sol
     Clones = 5  # 0xsplits clones
+
+    # https://github.com/safe-global/safe-contracts/blob/main/contracts/proxies/SafeProxy.sol
     GnosisSafe = 6
+
+    # https://github.com/OpenZeppelin/openzeppelin-labs/blob/master/initializer_contracts/contracts/UpgradeabilityProxy.sol
     OpenZeppelin = 7  # openzeppelin upgradeability proxy
+
+    # https://eips.ethereum.org/EIPS/eip-897
     Delegate = 8  # eip-897 delegate proxy
+
+    # https://medium.com/coinmonks/the-more-minimal-proxy-5756ae08ee48
     ZeroAge = 9  # a more-minimal proxy
-    SoladyPush0 = 10  # solady push0 minimal proxy
-    ClonesWithImmutableArgs = 11  # https://github.com/wighawag/clones-with-immutable-args/blob/master/src/ClonesWithImmutableArgs.sol  # noqa: E501
-    Create2ClonesWithImmutableArgs = 12  # https://github.com/emo-eth/create2-clones-with-immutable-args/blob/main/src/Create2ClonesWithImmutableArgs.sol  # noqa: E501
+
+    # https://github.com/wighawag/clones-with-immutable-args/blob/master/src/ClonesWithImmutableArgs.sol
+    # https://github.com/emo-eth/create2-clones-with-immutable-args/blob/main/src/Create2ClonesWithImmutableArgs.sol
+    CWIA = 10  # clones with immutable args
+
+    # https://github.com/Vectorized/solady/blob/main/src/utils/LibClone.sol
+    SoladyPush0 = 11  # solady push0 minimal proxy
+    SoladyCWIA = 12  # clones with immutable args with a receive method
 
 
 class ProxyInfo(ProxyInfoAPI):
