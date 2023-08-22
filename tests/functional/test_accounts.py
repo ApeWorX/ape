57411239431da9d2e4a36199a2065a3f92b5b6c3
z80 <z80@ophy.xyz>
2023-08-22 17:41:24 -0400

    feat: Return None for unsupported type, log warning

--- tests/functional/test_accounts.py
+++ tests/functional/test_accounts.py
@@ -53,6 +53,12 @@ def test_sign_message(signer, message):
     assert signer.check_signature(message, signature)
 
 
+def test_sign_message_unsupported_type_returns_none(signer):
+    message = 1234
+    signature = signer.sign_message(message)
+    assert signature is None
+
+
 def test_recover_signer(signer, message):
     signature = signer.sign_message(message)
     assert recover_signer(message, signature) == signer
