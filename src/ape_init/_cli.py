d361b1bd1faec7bee746fdfe649710f065416f6c
SAIKAT KARMAKAR <saikickkarma@gmail.com>
2023-08-23 01:34:16 +0100

    feat: added option to generate basic ape-config.yaml file if [tool.ape.plugins] property is specified inside pyproject.toml file

--- src/ape_init/_cli.py
+++ src/ape_init/_cli.py
@@ -7,6 +7,32 @@ from ape.cli import ape_cli_context
 from ape.utils import github_client
 
 
+def read_dependencies_from_toml(file_path):
+    with open(file_path, "r") as file:
+        content = file.readlines()
+
+    in_dependencies = False
+    dependencies = []
+
+    for line in content:
+        stripped_line = line.strip()
+        if "[tool.ape.plugins]" in stripped_line:
+            in_dependencies = True
+        elif "[" in stripped_line and "]" in stripped_line:
+            in_dependencies = False
+        elif in_dependencies and stripped_line.startswith("ape-"):
+            dependencies.append(stripped_line.split("=")[0][4:])
+
+    return dependencies
+
+
+def write_ape_config_yml(dependencies, file_to_write):
+    with open(file_to_write, "w") as file:
+        file.write("plugins:\n")
+        for dependency in dependencies:
+            file.write(f"  - name: {dependency}\n")
+
+
 @click.command(short_help="Initalize an ape project")
 @ape_cli_context()
 @click.option("--github", metavar="github-org/repo", help="Clone a template from Github")
@@ -59,3 +85,11 @@ __pycache__
             project_name = click.prompt("Please enter project name")
             ape_config.write_text(f"name: {project_name}\n")
             cli_ctx.logger.success(f"{project_name} is written in ape-config.yaml")
+
+        pyproject_toml = project_folder / "pyproject.toml"
+        if pyproject_toml.exists():
+            dependencies = read_dependencies_from_toml(pyproject_toml)
+            write_ape_config_yml(dependencies, ape_config)
+            cli_ctx.logger.success(
+                f"Generated {ape_config} based on the currect pyproject.toml file"
+            )
