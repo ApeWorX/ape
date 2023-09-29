3554a265335af496e9f028633382e7b833ce317e
z80 <z80@ophy.xyz>
2023-09-29 00:43:44 -0400

    fix: decode salt bytes before printing

--- src/ape_accounts/accounts.py
+++ src/ape_accounts/accounts.py
@@ -183,7 +183,7 @@ class KeyfileAccount(AccountAPI):
             if msg._verifyingContract_:
                 display_msg += f"\tContract: {msg._verifyingContract_}\n"
             if msg._salt_:
-                display_msg += f"\tSalt: {msg._salt_}\n"
+                display_msg += f"\tSalt: {msg._salt_.decode('utf-8')}\n"
 
             # Message Data
             display_msg += "Message\n"
