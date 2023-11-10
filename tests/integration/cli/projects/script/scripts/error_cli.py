69deb2ec3c44d9b645c1e43693e674143475be9b
z80 <z80@ophy.xyz>
2023-11-10 15:43:46 -0500

    fix: add test verifying state persists

--- tests/integration/cli/projects/script/scripts/error_cli.py
+++ tests/integration/cli/projects/script/scripts/error_cli.py
@@ -1,7 +1,12 @@
 import click
 
+import ape
+
 
 @click.command(short_help="Use a subcommand")
 def cli():
     local_variable = "test foo bar"  # noqa[F841]
+    acct = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
+    provider = ape.chain.provider
+    provider.set_balance(acct, 100000000)
     raise Exception("Expected exception")  # noqa: T001
--- tests/integration/cli/projects/script/scripts/error_no_def.py
+++ tests/integration/cli/projects/script/scripts/error_no_def.py
@@ -1,2 +1,7 @@
+import ape
+
 local_variable = "test foo bar"  # noqa[F841]
+acct = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
+provider = ape.chain.provider
+provider.set_balance(acct, 100000000)
 raise Exception("Expected exception")
