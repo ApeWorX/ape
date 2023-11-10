f764b80be056a090663f9d9784d179e1ba8efd48
z80 <z80@ophy.xyz>
2023-11-10 16:09:33 -0500

    fix: update test to use set_timestamp instead

--- tests/integration/cli/projects/script/scripts/error_main.py
+++ tests/integration/cli/projects/script/scripts/error_main.py
@@ -3,7 +3,6 @@ import ape
 
 def main():
     local_variable = "test foo bar"  # noqa[F841]
-    acct = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
     provider = ape.chain.provider
-    provider.set_balance(acct, 100000000)
+    provider.set_timestamp(123123123123123123)
     raise Exception("Expected exception")
