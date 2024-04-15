a2d06335a3b90c01f9f5321e510e27dd12b5348d
Mihai <mcosma@gmail.com>
2024-04-15 02:19:57 -0400

    chore: uncap eth_account

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ setup(
         "watchdog>=3.0,<4",
         # ** Dependencies maintained by Ethereum Foundation **
         "eth-abi>=5.1.0,<6",
-        "eth-account>=0.10.0,<0.11",
+        "eth-account>=0.10.0",
         "eth-tester @ git+https://github.com/wakamex/eth-tester.git",
         "eth-typing>=3.5.2,<4",
         "eth-utils>=2.3.1",
