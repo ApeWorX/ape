04e257f1331dcb43797f437c56d8c01a885594ba
Mihai <mcosma@gmail.com>
2024-04-15 18:29:23 -0400

    lint with black

--- src/ape_plugins/_cli.py
+++ src/ape_plugins/_cli.py
@@ -191,7 +191,7 @@ def uninstall(cli_ctx, plugins, skip_confirmation):
             skip_confirmation or click.confirm(f"Remove plugin '{plugin}'?")
         ):
             cli_ctx.logger.info(f"Uninstalling '{plugin.name}'...")
-            args = [*PIP_COMMAND,"uninstall", "-y", plugin.package_name, "--quiet"]
+            args = [*PIP_COMMAND, "uninstall", "-y", plugin.package_name, "--quiet"]
 
             # NOTE: Be *extremely careful* with this command, as it modifies the user's
             #       installed packages, to potentially catastrophic results
