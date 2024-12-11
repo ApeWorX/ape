6ca9d386e4860f12b2ed4326aedf48b429b04481
0xthedance <150128373+0xthedance@users.noreply.github.com>
2024-12-11 15:25:23 +0000

    fix: issue where gnosis-safe proxy sometimes was not detected (#2400)
    Co-authored-by: antazoey <admin@antazoey.me>

---
+++ tests/functional/data/sources/SafeProxy.sol
@@ -0,0 +1,47 @@
+/ SPDX-License-Identifier: LGPL-3.0-only
+/* solhint-disable one-contract-per-file */
+
+pragma solidity >=0.7.0 <0.8.0;
+
+
+//copied from https://github.com/safe-global/safe-smart-account/blob/main/contracts/proxies/SafeProxy.sol
+
+contract SafeProxy {
+    // Singleton always needs to be first declared variable, to ensure that it is at the same location in the contracts to which calls are delegated.
+    // To reduce deployment costs this variable is internal and needs to be retrieved via `getStorageAt`
+    address internal singleton;
+
+    /**
+     * @notice Constructor function sets address of singleton contract.
+     * @param _singleton Singleton address.
+     */
+    constructor(address _singleton) {
+        require(_singleton != address(0), "Invalid singleton address provided");
+        singleton = _singleton;
+    }
+
+    /// @dev Fallback function forwards all transactions and returns all received return data.
+    fallback() external payable {
+        // Note that this assembly block is **intentionally** not marked as memory-safe. First of all, it isn't memory
+        // safe to begin with, and turning this into memory-safe assembly would just make it less gas efficient.
+        // Additionally, we noticed that converting this to memory-safe assembly had no affect on optimizations of other
+        // contracts (as it always gets compiled alone in its own compilation unit anyway).
+        /* solhint-disable no-inline-assembly */
+        assembly {
+            let _singleton := sload(0)
+            // 0xa619486e == keccak("masterCopy()"). The value is right padded to 32-bytes with 0s
+            if eq(calldataload(0), 0xa619486e00000000000000000000000000000000000000000000000000000000) {
+                mstore(0, shr(12, shl(12, _singleton)))
+                return(0, 0x20)
+            }
+            calldatacopy(0, 0, calldatasize())
+            let success := delegatecall(gas(), _singleton, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            if iszero(success) {
+                revert(0, returndatasize())
+            }
+            return(0, returndatasize())
+        }
+        /* solhint-enable no-inline-assembly */
+    }
+}
---
+++ tests/functional/geth/test_proxy.py
@@ -0,0 +1,106 @@
+from eth_pydantic_types import HexBytes
+
+from ape.contracts import ContractContainer
+from ape_ethereum.proxies import ProxyType
+from tests.conftest import geth_process_test
+
+
+@geth_process_test
+def test_standard_proxy(get_contract_type, owner, geth_contract, ethereum):
+    """
+    NOTE: Geth is used here because EthTester does not implement getting storage slots.
+    """
+    _type = get_contract_type("eip1967")
+    contract = ContractContainer(_type)
+    target = geth_contract.address
+    contract_instance = owner.deploy(contract, target, HexBytes(""))
+    actual = ethereum.get_proxy_info(contract_instance.address)
+    assert actual is not None
+    assert actual.type == ProxyType.Standard
+    assert actual.target == target
+
+
+@geth_process_test
+def test_beacon_proxy(get_contract_type, geth_contract, owner, ethereum):
+    """
+    NOTE: Geth is used here because EthTester does not implement getting storage slots.
+    """
+    _type = get_contract_type("beacon")
+    beacon_contract = ContractContainer(_type)
+    target = geth_contract.address
+    beacon_instance = owner.deploy(beacon_contract, target)
+    beacon = beacon_instance.address
+
+    _type = get_contract_type("BeaconProxy")
+    contract = ContractContainer(_type)
+    contract_instance = owner.deploy(contract, beacon, HexBytes(""))
+
+    actual = ethereum.get_proxy_info(contract_instance.address)
+    assert actual is not None
+    assert actual.type == ProxyType.Beacon
+    assert actual.target == target
+
+
+@geth_process_test
+def test_uups_proxy(get_contract_type, geth_contract, owner, ethereum):
+    _type = get_contract_type("Uups")
+    contract = ContractContainer(_type)
+
+    target = geth_contract.address
+
+    contract_instance = owner.deploy(contract, HexBytes("0x2beb1711"), target)
+
+    actual = ethereum.get_proxy_info(contract_instance.address)
+    assert actual is not None
+    assert actual.type == ProxyType.UUPS
+    assert actual.target == target
+
+
+@geth_process_test
+def test_gnosis_safe(get_contract_type, geth_contract, owner, ethereum):
+    _type = get_contract_type("SafeProxy")
+    contract = ContractContainer(_type)
+
+    target = geth_contract.address
+
+    contract_instance = owner.deploy(contract, target)
+
+    actual = ethereum.get_proxy_info(contract_instance.address)
+
+    assert actual is not None
+    assert actual.type == ProxyType.GnosisSafe
+    assert actual.target == target
+
+
+@geth_process_test
+def test_openzeppelin(get_contract_type, geth_contract, owner, ethereum, sender):
+    _type = get_contract_type("UpgradeabilityProxy")
+    contract = ContractContainer(_type)
+
+    target = geth_contract.address
+    _type = get_contract_type("SolFallbackAndReceive")
+    constructor_container = ContractContainer(_type)
+    constructor_contract = owner.deploy(constructor_container)
+
+    contract_instance = owner.deploy(contract, constructor_contract.address, target)
+
+    actual = ethereum.get_proxy_info(contract_instance.address)
+    assert actual is not None
+    assert actual.type == ProxyType.OpenZeppelin
+    assert actual.target == target
+
+
+@geth_process_test
+def test_delegate(get_contract_type, geth_contract, owner, ethereum):
+    _type = get_contract_type("ERCProxy")
+    contract = ContractContainer(_type)
+
+    target = geth_contract.address
+
+    contract_instance = owner.deploy(contract, target)
+
+    actual = ethereum.get_proxy_info(contract_instance.address)
+
+    assert actual is not None
+    assert actual.type == ProxyType.Delegate
+    assert actual.target == target
