14fa6f961329197ae6c7f2896095f7094cc8a6f3
antazoey <antyzoa@gmail.com>
2025-01-28 15:57:16 -0600

    test: stupid test shut up

--- tests/functional/test_proxy.py
+++ tests/functional/test_proxy.py
@@ -5,7 +5,8 @@ NOTE: Most proxy tests are in `geth/test_proxy.py`.
 """
 
 
-def test_minimal_proxy(ethereum, minimal_proxy, chain):
+def test_minimal_proxy(ethereum, minimal_proxy_container, chain, owner):
+    minimal_proxy = owner.deploy(minimal_proxy_container, sender=owner)
     chain.provider.network.__dict__["explorer"] = None  # Ensure no explorer, messes up test.
     actual = ethereum.get_proxy_info(minimal_proxy.address)
     assert actual is not None
