54e47f91fe2ad00eb0c4f7327fcc9806788ab66d
antazoey <antyzoa@gmail.com>
2025-02-07 11:35:24 -0600

    feat: ability to get conversion classes by name

--- src/ape/managers/converters.py
+++ src/ape/managers/converters.py
@@ -394,6 +394,23 @@ class ConversionManager(BaseManager):
 
         raise ConversionError(f"No conversion registered to handle '{value}'.")
 
+    def get_converter(self, name: str) -> ConverterAPI:
+        """
+        Get a converter plugin by name.
+
+        Args:
+            name (str): The name of the converter.
+
+        Returns:
+            :class:`~ape.api.converters.ConverterAPI`: The converter.
+        """
+        for converter_ls in self._converters.values():
+            for converter in converter_ls:
+                if converter.name == name.lower():
+                    return converter
+
+        raise ConversionError("No converters with name '{name}'.'")
+
     def convert_method_args(
         self,
         abi: Union["MethodABI", "ConstructorABI", "EventABI"],
