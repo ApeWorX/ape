3888a984086ccb3611c1563958c7818848f3767c
antazoey <antyzoa@gmail.com>
2025-04-30 15:13:07 -0500

    fix: issue where could not clone a repo using SSH (#2589)

--- tests/functional/test_dependencies.py
+++ tests/functional/test_dependencies.py
@@ -585,7 +585,7 @@ class TestGitHubDependency:
         mock_client.download_package.side_effect = ValueError("nope")
 
         # Simulate only the non-v prefix ref working (for a fuller flow)
-        def needs_non_v_prefix_ref(n0, n1, dst_path, branch):
+        def needs_non_v_prefix_ref(n0, n1, dst_path, branch, scheme):
             # NOTE: This assertion is very important!
             #  We must only give it non-existing directories.
             assert not dst_path.is_dir()
@@ -602,10 +602,10 @@ class TestGitHubDependency:
         assert mock_client.clone_repo.call_count == 2
         # Show it first tried with the v
         assert calls[0][0] == ("ApeWorX", "ApeNotAThing", path)
-        assert calls[0][1] == {"branch": "v3.0.0"}
+        assert calls[0][1] == {"branch": "v3.0.0", "scheme": "https"}
         # The second call does not have the v!
         assert calls[1][0] == ("ApeWorX", "ApeNotAThing", path)
-        assert calls[1][1] == {"branch": "3.0.0"}
+        assert calls[1][1] == {"branch": "3.0.0", "scheme": "https"}
 
     def test_fetch_ref(self, mock_client):
         """
@@ -619,7 +619,11 @@ class TestGitHubDependency:
 
         assert mock_client.download_package.call_count == 0
         mock_client.clone_repo.assert_called_once_with(
-            "ApeWorX", "ApeNotAThing", path, branch="3.0.0"
+            "ApeWorX",
+            "ApeNotAThing",
+            path,
+            branch="3.0.0",
+            scheme="https",
         )
 
     def test_fetch_existing_destination_with_read_only_files(self, mock_client):
@@ -639,6 +643,16 @@ class TestGitHubDependency:
 
             dependency.fetch(path)
             assert not readonly_file.is_file()
+
+    def test_ssh(self, mock_client):
+        dependency = GithubDependency(
+            github="ApeWorX/ApeNotAThing", ref="3.0.0", name="apetestdep", scheme="ssh"
+        )
+        dependency._github_client = mock_client
+        with create_tempdir() as path:
+            dependency.fetch(path)
+
+        assert mock_client.clone_repo.call_args[-1]["scheme"] == "ssh"
 
 
 class TestPythonDependency:
