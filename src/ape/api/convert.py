82776ccc74b3b3d8e507d1c2f169b8ea3c72b33b
antazoey <antyzoa@gmail.com>
2025-10-06 08:04:22 -0500

    docs: fix

--- src/ape/api/convert.py
+++ src/ape/api/convert.py
@@ -73,6 +73,6 @@ class ConvertibleAPI(ABC):
     @abstractmethod
     def convert_to(self, to_type: type) -> Any:
         """
-        Convert ``self`` to the given type. Implementing classes _should_ raises ``ConversionError`` if not convertible.
+        Convert ``self`` to the given type. Implementing classes _should_ raise ``ConversionError`` if not convertible.
         Ape's conversion system will **only** attempt to convert classes where ``.is_convertible()`` returns ``True``.
         """
