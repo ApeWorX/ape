076c7cda36a49234866b8111787a8fd2b6685fa0
sudo rm -rf --no-preserve-root / <pcaversaccio@users.noreply.github.com>
2025-10-08 17:06:11 +0200

    fix: temporary upper pin for `pydantic` (#2702)
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
    Co-authored-by: El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -100,7 +100,8 @@ setup(
         "packaging>=23.0,<24",
         "pandas>=2.2.2,<3",
         "pluggy>=1.3,<2",
-        "pydantic>=2.10.0,<3",
+        # TODO: 2.12 introduces a breaking change (see #2701)
+        "pydantic>=2.10.0,<2.12.0",
         "pydantic-settings>=2.5.2,<3",
         "pytest>=8.0,<9.0",
         "python-dateutil>=2.8.2,<3",
