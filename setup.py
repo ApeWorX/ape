4055df47412889ec673e8f1cbdcf86e21769ebbd
Vladislav <4pex1oh@gmail.com>
2025-12-16 00:04:18 +0700

    chore: remove python 3.14 from CI (because of  #2733 )

--- setup.py
+++ setup.py
@@ -166,6 +166,5 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
     ],
 )
