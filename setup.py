b49becb610ffdaa6e6b2c0bbf33e4b411d7d287c
Vladislav <4pex1oh@gmail.com>
2025-12-16 19:15:36 +0700

    chore: align CI/docs with Python>=3.10

--- setup.py
+++ setup.py
@@ -162,7 +162,6 @@ setup(
         "Operating System :: MacOS",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
